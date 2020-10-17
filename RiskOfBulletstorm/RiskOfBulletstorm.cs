2f576adb1b7fa07b327a31564f90c3fdb4b11b49
DestroyedClone <DestroyedClone@gmail.com>
2020-10-16 22:19:21 -0700

    simplified

--- RiskOfBulletstorm/RiskOfBulletstorm.cs
+++ RiskOfBulletstorm/RiskOfBulletstorm.cs
@@ -1,19 +1,18 @@
 ﻿using BepInEx;
 using BepInEx.Configuration;
-using R2API;
-using R2API.Utils;
-using System.Reflection;
-using UnityEngine;
+//using R2API;
+//using R2API.Utils;
+//using System.Reflection;
+//using UnityEngine;
 using Path = System.IO.Path;
 using TILER2;
 using static TILER2.MiscUtil;
-using RoR2;
-using R2API.AssetPlus;
+//using RoR2;
+//using R2API.AssetPlus;
 
 namespace DestroyedClone
 {
     [BepInDependency("com.bepis.r2api")]
-    //Change these
     [BepInPlugin(ModGuid, ModName, ModVer)]
     public class RiskofBulletstorm : BaseUnityPlugin
     {
@@ -21,17 +20,17 @@ namespace DestroyedClone
         public const string ModName = "Risk of Bulletstorm";
         public const string ModGuid = "com.DestroyedClone.RiskOfBulletstorm";
 
-        private static ConfigFile ConfigFile;
+        //private static ConfigFile ConfigFile;
         internal static FilingDictionary<ItemBoilerplate> masterItemList = new FilingDictionary<ItemBoilerplate>();
         private void Awake()
         {
-            ConfigFile = new ConfigFile(Path.Combine(Paths.ConfigPath, ModGuid + ".cfg"), true);
+            //ConfigFile = new ConfigFile(Path.Combine(Paths.ConfigPath, ModGuid + ".cfg"), true);
             masterItemList = ItemBoilerplate.InitAll("RiskofBulletstorm");
-
+            /*
             foreach (ItemBoilerplate x in masterItemList)
             {
                 x.SetupConfig(ConfigFile);
-            }
+            } */
 
             foreach (ItemBoilerplate x in masterItemList)
             {
