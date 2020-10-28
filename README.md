4bb782609a13d281d28ce668679894108d3a66ed
DestroyedClone <DestroyedClone@gmail.com>
2020-10-28 11:30:48 -0700

    Added Ticket, Adjusted PlusBullets default values.

--- README.md
+++ README.md
@@ -4,14 +4,39 @@ A mod for Risk of Rain 2. Built with BepInEx and R2API and TILER2
 
 Plans to add a bunch of custom items, modified from Enter the Gungeon.
 
-Installation of TILER2 is also required: https://thunderstore.io/package/ThinkInvis/TILER2/
+# Dependencies
+
+ThinkIvis - TILER2
+Jarlyk - EliteSpawningOverhaul
 
 # Items
-Modifications include, player on-hit changed to % taken.
+No assets currently.
 
-Enraging Photo: Gain a temporary 200% damage boost upon taking 12% of your health in damage for 3 seconds.
+#[Items]
+[unimplemented] Bloodied Scarf: Replaces your utility with Huntress Blink
 
-=> Change: 12% health taken instead of on-hit
+[Green] Enraging Photo: Gain a temporary +100% damage boost (+50% per stack) upon taking 12% of your health in damage for 3 seconds.
+=> TODO: Assets
+=> TODO: Health Requirement Loss
+=> TODO: Set Tier to Green
+=> BetterUI Compat
+
+[unimplemented] Green Guon Stone: 
+
+[Green] +1 Bullets: +25% damage (+5% per stack)
+
+[]
+
+#[Equipment]
+
+#[Curse]
+[unimplemented] Picking up RiskOfBulletstorm items that defy Kaliber adds +0.25 curse. Each subsequent stack of a single item will add +0.1.
+Curse increases the chance of an enemy becoming Jammed.
+
+#[Elites]
+Kaliber's Wrath - Jammed {0}
+Jammed Elites deal 300% ([unimplemented]+20% per stage) damage. This damage scaling better reflects RoR2's health scaling vs Gungeon's.
+
 
 ## Credits
 Nearly all the code either directly references or is directly copied from and modified. Including this readme:
@@ -20,4 +45,5 @@ KomradeSpectre - AetheriumMod https://github.com/KomradeSpectre/AetheriumMod
 
 ThinkInvis - ClassicItems https://github.com/ThinkInvis/RoR2-ClassicItems
 
-I'm crediting you anyways https://github.com/cheeeeeeeeeen/RoR2-ClassicItems
+Chen - Chen's ClassicItems https://github.com/cheeeeeeeeeen/RoR2-ClassicItems
+
--- RiskOfBulletstorm/Items/Ticket.cs
+++ RiskOfBulletstorm/Items/Ticket.cs
@@ -13,28 +13,27 @@ using static TILER2.MiscUtil;
 
 namespace RiskOfBulletstorm.Items
 {
-    public class PlusBullets : Item_V2<PlusBullets>
+    public class Ticket : Equipment_V2<Ticket>
     {
         //TODO: USE CHEN's HEALTH LOSS CODE FOR FLOATS
         [AutoConfigUpdateActions(AutoConfigUpdateActionTypes.InvalidateLanguage)]
-        [AutoConfig("How many damage should [+1 Bullets] provide with a single stack? (Default: 0.22 = 20% dmg)", AutoConfigFlags.PreventNetMismatch)]
-        public float DamageBonus { get; private set; } = 0.2f;
+        [AutoConfig("How many damage should [+1 Bullets] provide with a single stack? (Default: 0.25 = 25% dmg)", AutoConfigFlags.PreventNetMismatch)]
+        public float DamageBonus { get; private set; } = 0.25f;
 
         [AutoConfigUpdateActions(AutoConfigUpdateActionTypes.InvalidateLanguage)]
-        [AutoConfig("How many additional damage should each [+1 Bullets] after the first give? (Default: 0.02 = 2% damage)", AutoConfigFlags.PreventNetMismatch)]
-        public float DamageBonusStack { get; private set; } = 0.02f;
+        [AutoConfig("How many additional damage should each [+1 Bullets] after the first give? (Default: 0.05 = 5% damage)", AutoConfigFlags.PreventNetMismatch)]
+        public float DamageBonusStack { get; private set; } = 0.05f;
 
-        public override string displayName => "+1 Bullets";
-        public override ItemTier itemTier => ItemTier.Tier1;
-        public override ReadOnlyCollection<ItemTag> itemTags => new ReadOnlyCollection<ItemTag>(new[] { ItemTag.Damage });
+        public override string displayName => "Ticket";
+        public override float cooldown { get; protected set; } = 45f;
 
         protected override string GetNameString(string langID = null) => displayName;
 
-        protected override string GetPickupString(string langID = null) => "+1 To Bullet.";
+        protected override string GetPickupString(string langID = null) => "Do You Have Yours?";
 
-        protected override string GetDescString(string langid = null) => $"Increases damage by <style=cIsDamage>{Pct(DamageBonus)}% damage</style>. <style=cIsDamage>+{Pct(DamageBonusStack)}% damage</style> per stack.";
+        protected override string GetDescString(string langid = null) => $"Summons Gatling Gull as an ally.";
 
-        protected override string GetLoreString(string langID = null) => "Masterwork bullets.\n\nPeer-reviewed studies have shown that these bullets are precisely 1 better than normal bullets.";
+        protected override string GetLoreString(string langID = null) => "Gatling Gull respects martial prowess in Gungeoneers. Spend this ticket to bring in the big guns.";
 
         //private static List<RoR2.CharacterBody> Playername = new List<RoR2.CharacterBody>();
 
