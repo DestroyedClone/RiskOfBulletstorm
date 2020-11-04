0fef18ca2c2c662eaa630bea17329aca550c6304
DestroyedClone <DestroyedClone@gmail.com>
2020-11-03 16:25:47 -0800

    Added Battle Standard skeleton

---
+++ RiskOfBulletstorm/Items/Tier2/BattleStandard.cs
@@ -0,0 +1,54 @@
+﻿//using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+//using System.Text;
+using R2API;
+using RoR2;
+using RoR2.UI;
+using UnityEngine;
+using UnityEngine.Networking;
+using TILER2;
+using static TILER2.StatHooks;
+using static TILER2.MiscUtil;
+using System;
+
+namespace RiskOfBulletstorm.Items
+{
+    public class BattleStandard : Item_V2<BattleStandard> //Change to equipment that gives cursed.
+    {
+        public override string displayName => "Battle Standard";
+        public override ItemTier itemTier => ItemTier.Tier2;
+        public override ReadOnlyCollection<ItemTag> itemTags => new ReadOnlyCollection<ItemTag>(new[] { ItemTag.Damage });
+
+        protected override string GetNameString(string langID = null) => displayName;
+        protected override string GetPickupString(string langID = null) => "Set Your Own!" +
+            "\nImproves the effectiveness of companions and charmed enemies.";
+
+        protected override string GetDescString(string langid = null) => $"";
+
+        protected override string GetLoreString(string langID = null) => "Before the advancement of Gundead society, belligerent bands of Bullet Kin would rally around these banners before their raids upon the upper chambers.";
+
+        public override void SetupBehavior()
+        {
+
+        }
+        public override void SetupAttributes()
+        {
+            base.SetupAttributes();
+
+        }
+        public override void SetupConfig()
+        {
+            base.SetupConfig();
+        }
+        public override void Install()
+        {
+            base.Install();
+        }
+
+        public override void Uninstall()
+        {
+            base.Uninstall();
+        }
+    }
+}
