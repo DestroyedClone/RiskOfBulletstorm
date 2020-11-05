9a772b77a4a0f8948bdd096cedc8b1547b4988a3
DestroyedClone <DestroyedClone@gmail.com>
2020-11-05 00:51:44 -0800

    Added skeleton for GhostBullets

---
+++ RiskOfBulletstorm/Items/Tier2/Ghostbullets.cs
@@ -0,0 +1,62 @@
+﻿//using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+//using System.Text;
+using R2API;
+using RoR2;
+using RoR2.UI;
+using UnityEngine;
+using UnityEngine.Networking;
+using TILER2;
+using static TILER2.StatHooks;
+using static TILER2.MiscUtil;
+using System;
+
+namespace RiskOfBulletstorm.Items
+{
+    public class GhostBullets : Item_V2<GhostBullets>
+    {
+        public override string displayName => "Ghost Bullets";
+        public override ItemTier itemTier => ItemTier.Tier2;
+        public override ReadOnlyCollection<ItemTag> itemTags => new ReadOnlyCollection<ItemTag>(new[] { ItemTag.Damage });
+
+        protected override string GetNameString(string langID = null) => displayName;
+        protected override string GetPickupString(string langID = null) => "Shoot Through\nGrants all bullets piercing.";
+
+        protected override string GetDescString(string langid = null) => $"All projectiles and bullets can pierce 1(+1) targets";
+
+        protected override string GetLoreString(string langID = null) => "";
+
+        public override void SetupBehavior()
+        {
+
+        }
+        public override void SetupAttributes()
+        {
+            base.SetupAttributes();
+
+        }
+        public override void SetupConfig()
+        {
+            base.SetupConfig();
+        }
+        public override void Install()
+        {
+            base.Install();
+            On.RoR2.Projectile.ProjectileManager.FireProjectile_FireProjectileInfo += PM_FireProj;
+        }
+
+        public override void Uninstall()
+        {
+            base.Uninstall();
+            On.RoR2.Projectile.ProjectileManager.FireProjectile_FireProjectileInfo -= PM_FireProj;
+        }
+
+        private void PM_FireProj(On.RoR2.Projectile.ProjectileManager.orig_FireProjectile_FireProjectileInfo orig, RoR2.Projectile.ProjectileManager self, RoR2.Projectile.FireProjectileInfo fireProjectileInfo)
+        {
+            //fireProjectileInfo.
+            orig(self, fireProjectileInfo);
+
+        }
+    }
+}
