9ee8a679b9689a2295e833ffa372b763090740b0
DestroyedClone <DestroyedClone@gmail.com>
2020-11-18 11:35:08 -0800

    git >:(

---
+++ RiskOfBulletstorm/Releases/Items/Bomb.cs
@@ -0,0 +1,228 @@
+﻿using EntityStates.Engi.EngiWeapon;
+using R2API;
+using RoR2;
+using RoR2.Projectile;
+using UnityEngine;
+using UnityEngine.Networking;
+using TILER2;
+using static TILER2.MiscUtil;
+
+namespace RiskOfBulletstorm.Items
+{
+    public class Bomb : Equipment_V2<Bomb>
+    {
+        [AutoConfigUpdateActions(AutoConfigUpdateActionTypes.InvalidateLanguage)]
+        [AutoConfig("Damage? (Default: 1.0 = 100% damage)", AutoConfigFlags.PreventNetMismatch)]
+        public float DamageDealt { get; private set; } = 1f;
+
+        //[AutoConfigUpdateActions(AutoConfigUpdateActionTypes.InvalidateLanguage)]
+        //[AutoConfig("Cooldown? (Default: 8 = 8 seconds)", AutoConfigFlags.PreventNetMismatch)]
+        //public float Cooldown_config { get; private set; } = 8f;
+
+        public override string displayName => "Bomb";
+        public string descText = "Throws a bomb that explodes after a short delay";
+        public override float cooldown { get; protected set; } = 8f;
+
+        protected override string GetNameString(string langID = null) => displayName;
+
+        protected override string GetPickupString(string langID = null) => "Use For Boom\n"+descText;
+
+        protected override string GetDescString(string langid = null) => $"{descText}, dealing {Pct(DamageDealt)} damage.";
+
+        protected override string GetLoreString(string langID = null) => "Countless experienced adventurers have brought Bombs to the Gungeon seeking secret doors, only to be foiled by the existence of Blanks. Still, explosives have their place.";
+
+        public static GameObject BombPrefab { get; private set; }
+
+        public static GameObject ItemBodyModelPrefab;
+
+        public Bomb()
+        {
+            modelResourcePath = "@RiskOfBulletstorm:Assets/Models/Prefabs/Bomb.prefab";
+            iconResourcePath = "@RiskOfBulletstorm:Assets/Textures/Icons/BombIcon.png";
+        }
+
+        public override void SetupBehavior()
+        {
+            base.SetupBehavior();
+            GameObject commandoGrenadePrefab = Resources.Load<GameObject>("prefabs/projectiles/CommandoGrenadeProjectile");
+            BombPrefab = commandoGrenadePrefab.InstantiateClone("Bomb");
+            //BombPrefab.transform.localScale = new Vector3(3, 3, 3);
+            //BombPrefab.GetComponent<ProjectileSimple>().velocity = 0; //default 50
+            //BombPrefab.GetComponent<ProjectileSimple>().lifetime = 6; //default 5
+            BombPrefab.GetComponent<ProjectileDamage>().damageColorIndex = DamageColorIndex.Item;
+            BombPrefab.GetComponent<ProjectileImpactExplosion>().falloffModel = BlastAttack.FalloffModel.None;
+            Object.Destroy(BombPrefab.GetComponent<ApplyTorqueOnStart>());
+
+        }
+        public override void SetupAttributes()
+        {
+            if (ItemBodyModelPrefab == null)
+            {
+                ItemBodyModelPrefab = Resources.Load<GameObject>("@RiskOfBulletstorm:Assets/Models/Prefabs/Bomb.prefab");
+                displayRules = GenerateItemDisplayRules();
+            }
+            base.SetupAttributes();
+        }
+        private static ItemDisplayRuleDict GenerateItemDisplayRules()
+        {
+            ItemBodyModelPrefab.AddComponent<ItemDisplay>();
+            //ItemBodyModelPrefab.GetComponent<ItemDisplay>().rendererInfos = ItemHelpers.ItemDisplaySetup(ItemBodyModelPrefab);
+
+            Vector3 generalScale = new Vector3(0.4f, 0.4f, 0.4f);
+            ItemDisplayRuleDict rules = new ItemDisplayRuleDict(new ItemDisplayRule[]
+            {
+                new ItemDisplayRule
+                {
+                    ruleType = ItemDisplayRuleType.ParentedPrefab,
+                    followerPrefab = ItemBodyModelPrefab,
+                    childName = "Chest",
+                    localPos = new Vector3(0.75f, 0.5f, 0),
+                    localAngles = new Vector3(0, 0, 0),
+                    localScale = new Vector3(0.1f, 0.1f, 0.1f)
+                }
+            });
+            rules.Add("mdlHuntress", new ItemDisplayRule[]
+            {
+                new ItemDisplayRule
+                {
+                    ruleType = ItemDisplayRuleType.ParentedPrefab,
+                    followerPrefab = ItemBodyModelPrefab,
+                    childName = "Chest",
+                    localPos = new Vector3(0.75f, 0.5f, 0),
+                    localAngles = new Vector3(0, 0, 0),
+                    localScale = new Vector3(0.1f, 0.1f, 0.1f)
+                }
+            });
+            rules.Add("mdlToolbot", new ItemDisplayRule[]
+            {
+                new ItemDisplayRule
+                {
+                    ruleType = ItemDisplayRuleType.ParentedPrefab,
+                    followerPrefab = ItemBodyModelPrefab,
+                    childName = "Chest",
+                    localPos = new Vector3(-0.1f, -0.3f, 3.3f),
+                    localAngles = new Vector3(0f, 180f, 0f),
+                    localScale = generalScale
+                }
+            });
+            rules.Add("mdlEngi", new ItemDisplayRule[]
+            {
+                new ItemDisplayRule
+                {
+                    ruleType = ItemDisplayRuleType.ParentedPrefab,
+                    followerPrefab = ItemBodyModelPrefab,
+                    childName = "Chest",
+                    localPos = new Vector3(0f, 0.1f, 0.29f),
+                    localAngles = new Vector3(10f, 180f, 0f),
+                    localScale = generalScale
+                }
+            });
+            rules.Add("mdlMage", new ItemDisplayRule[]
+            {
+                new ItemDisplayRule
+                {
+                    ruleType = ItemDisplayRuleType.ParentedPrefab,
+                    followerPrefab = ItemBodyModelPrefab,
+                    childName = "Chest",
+                    localPos = new Vector3(0f, 0.025f, 0.19f),
+                    localAngles = new Vector3(11f, 180f, 0f),
+                    localScale = generalScale * 0.9f
+                }
+            });
+            rules.Add("mdlMerc", new ItemDisplayRule[]
+            {
+                new ItemDisplayRule
+                {
+                    ruleType = ItemDisplayRuleType.ParentedPrefab,
+                    followerPrefab = ItemBodyModelPrefab,
+                    childName = "Chest",
+                    localPos = new Vector3(0f, 0f, 0.2f),
+                    localAngles = new Vector3(0f, 180f, 0f),
+                    localScale = generalScale
+                }
+            });
+            rules.Add("mdlTreebot", new ItemDisplayRule[]
+            {
+                new ItemDisplayRule
+                {
+                    ruleType = ItemDisplayRuleType.ParentedPrefab,
+                    followerPrefab = ItemBodyModelPrefab,
+                    childName = "Base",
+                    localPos = new Vector3(0f, 0.4f, -1.6f),
+                    localAngles = new Vector3(0f, 180f, 90f),
+                    localScale = generalScale
+                }
+            });
+            rules.Add("mdlLoader", new ItemDisplayRule[]
+            {
+                new ItemDisplayRule
+                {
+                    ruleType = ItemDisplayRuleType.ParentedPrefab,
+                    followerPrefab = ItemBodyModelPrefab,
+                    childName = "Chest",
+                    localPos = new Vector3(0f, 0.05f, 0.3f),
+                    localAngles = new Vector3(0f, 180f, 0f),
+                    localScale = generalScale
+                }
+            });
+            rules.Add("mdlCroco", new ItemDisplayRule[]
+            {
+                new ItemDisplayRule
+                {
+                    ruleType = ItemDisplayRuleType.ParentedPrefab,
+                    followerPrefab = ItemBodyModelPrefab,
+                    childName = "Chest",
+                    localPos = new Vector3(0f, 0.5f, -2.6f),
+                    localAngles = new Vector3(-20f, 180f, 0f),
+                    localScale = generalScale
+                }
+            });
+            rules.Add("mdlCaptain", new ItemDisplayRule[]
+            {
+                new ItemDisplayRule
+                {
+                    ruleType = ItemDisplayRuleType.ParentedPrefab,
+                    followerPrefab = ItemBodyModelPrefab,
+                    childName = "Chest",
+                    localPos = new Vector3(0.05f, 0.1f, 0.22f),
+                    localAngles = new Vector3(5f, 210f, 0f),
+                    localScale = generalScale
+                }
+            });
+            return rules;
+        }
+        public override void SetupConfig()
+        {
+            base.SetupConfig();
+        }
+        public override void Install()
+        {
+            base.Install();
+        }
+
+        public override void Uninstall()
+        {
+            base.Uninstall();
+        }
+        protected override bool PerformEquipmentAction(EquipmentSlot slot)
+        {
+            CharacterBody body = slot.characterBody;
+            if (!body) return false;
+
+            Vector3 corePos = Util.GetCorePosition(body);
+            GameObject GameObject = slot.gameObject;
+            var input = body.inputBank;
+            Vector3 offset = new Vector3(0,0.5f,0);
+
+            Util.PlaySound(FireMines.throwMineSoundString, GameObject);
+            if (NetworkServer.active)
+            {
+                ProjectileManager.instance.FireProjectile(BombPrefab, corePos+offset, Util.QuaternionSafeLookRotation(input.aimDirection),
+                                      GameObject, body.damage * DamageDealt,
+                                      0f, Util.CheckRoll(body.crit, body.master),
+                                      DamageColorIndex.Item, null, -1f);
+            }
+            return true;
+        }
+    }
+}
---
+++ RiskOfBulletstorm/Releases/Items/KaliberWrath.cs
@@ -0,0 +1,100 @@
+﻿
+using EliteSpawningOverhaul;
+using R2API;
+using RoR2;
+using TILER2;
+using UnityEngine;
+using static TILER2.StatHooks;
+using static TILER2.MiscUtil;
+
+namespace RiskOfBulletstorm //TODO modify weight of card depending on curse level.
+{
+    public class KaliberWrath : Equipment_V2<KaliberWrath>
+    {
+        [AutoConfigUpdateActions(AutoConfigUpdateActionTypes.InvalidateLanguage)]
+        [AutoConfig("How much additional damage should a Jammed enemy deal?(Default: 2 (+200%))", AutoConfigFlags.PreventNetMismatch)]
+        public float DamageBoost { get; private set; } = 2.00f;
+        [AutoConfigUpdateActions(AutoConfigUpdateActionTypes.InvalidateLanguage)]
+        [AutoConfig("How much additional crit should a Jammed enemy have?(Default: 1 (+100%))", AutoConfigFlags.PreventNetMismatch)]
+        public float CritBoost { get; private set; } = 1f;
+        public override string displayName => "Wrath of Kaliber";
+
+        protected override string GetNameString(string langID = null) => displayName;
+        protected override string GetPickupString(string langID = null) => "Become an aspect of defiance";
+        protected override string GetDescString(string langid = null) => $"+{Pct(DamageBoost)} damage dealt. +{CritBoost} crit bonus.";
+        protected override string GetLoreString(string langID = null) => "Several quotes from what seems to be a form of holy scripture were found around the Gungeon." +
+            $"They reference the Gungeon's past, and made prophecies about its future." +
+            $"\nDue to the references to Kaliber that can be found in the texts, this book is most likely hers, however, it is incomplete." +
+            $"\nGunesis 1:1 In the beginning, the Gungeon was formless and void, and bullets moved over the face of the deep." +
+            $"\nRevolvations 2:1 These are the words of Kaliber, she who grips the seven sidearms in her hands and walks among the six loaded chambers." +
+            $"\n\nWhen the Gun is drawn to the heart of the pacifist, and when the chambers are empty and the shells are spent, the Last of the Jammed will ascend to seal The Breach forever.";
+
+        public static EliteAffixCard JammedEliteCard { get; set; }
+        public static EliteIndex JammedEliteIndex;
+        public static BuffIndex JammedBuffIndex;
+
+        public override void SetupAttributes()
+        {
+            base.SetupAttributes();
+            equipmentDef.canDrop = true;
+            equipmentDef.enigmaCompatible = false;
+
+            var jammedEliteDef = new CustomElite(
+            new EliteDef
+            {
+                name = "Jammed",
+                modifierToken = "ROB_ELITE_MODIFIER_JAMMED",
+                color = new Color32(150, 10, 10, 255),
+                eliteEquipmentIndex = equipmentDef.equipmentIndex
+            }, 1);
+            JammedEliteIndex = EliteAPI.Add(jammedEliteDef);
+            LanguageAPI.Add(jammedEliteDef.EliteDef.modifierToken, "Jammed {0}");
+
+            var jammedBuffDef = new CustomBuff(
+            new BuffDef
+            {
+                name = "Affix_Jammed",
+                buffColor = new Color32(150, 10, 10, 255),
+                iconPath = "",
+                eliteIndex = JammedEliteIndex,
+                canStack = false
+            });
+            JammedBuffIndex = BuffAPI.Add(jammedBuffDef);
+            equipmentDef.passiveBuff = JammedBuffIndex;
+
+            JammedEliteCard = new EliteAffixCard
+            {
+                spawnWeight = 0.8f,
+                costMultiplier = 15.0f,
+                damageBoostCoeff = 1.0f,
+                healthBoostCoeff = 4.0f,
+                eliteOnlyScaling = 0.5f,
+                eliteType = JammedEliteIndex,
+            };
+            EsoLib.Cards.Add(JammedEliteCard);
+        }
+        public override void Install()
+        {
+            base.Install();
+            GetStatCoefficients += KaliberReward;
+
+        }
+        public override void Uninstall()
+        {
+            base.Uninstall();
+            GetStatCoefficients -= KaliberReward;
+        }
+        private void KaliberReward(CharacterBody sender, StatHookEventArgs args)
+        {
+            if (sender.HasBuff(JammedBuffIndex))
+            {
+                args.damageMultAdd += DamageBoost;
+                args.critAdd += CritBoost;
+            }
+        }
+        protected override bool PerformEquipmentAction(EquipmentSlot slot)
+        {
+            return false;
+        }
+    }
+}
