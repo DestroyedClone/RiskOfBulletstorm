1e48c5c310177e519e4299159672ad4c5629f30a
DestroyedClone <DestroyedClone@gmail.com>
2020-11-20 02:09:33 -0800

    Added renderinfos

---
+++ RiskOfBulletstorm/Util/ItemHelpers.cs
@@ -0,0 +1,28 @@
+﻿using RoR2;
+using UnityEngine;
+
+namespace RiskOfBulletstorm.Utils
+{
+    public class ItemHelpers
+    {
+        public static CharacterModel.RendererInfo[] ItemDisplaySetup(GameObject obj)
+        {
+            MeshRenderer[] meshes = obj.GetComponentsInChildren<MeshRenderer>();
+            CharacterModel.RendererInfo[] renderInfos = new CharacterModel.RendererInfo[meshes.Length];
+
+            for (int i = 0; i < meshes.Length; i++)
+            {
+                renderInfos[i] = new CharacterModel.RendererInfo
+                {
+                    defaultMaterial = meshes[i].material,
+                    renderer = meshes[i],
+                    defaultShadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On,
+                    ignoreOverlays = false //We allow the mesh to be affected by overlays like OnFire or PredatoryInstinctsCritOverlay.
+                };
+            }
+
+            return renderInfos;
+
+        }
+    }
+}
