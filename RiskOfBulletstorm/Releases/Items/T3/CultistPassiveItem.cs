e795918bcef3861449c7077c9de48852a29679f4
DestroyedClone <DestroyedClone@gmail.com>
2020-11-21 23:34:02 -0800

    Added Number 2 item

---
+++ RiskOfBulletstorm/Releases/Items/T3/CultistPassiveItem.cs
@@ -0,0 +1,53 @@
+﻿//using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+//using System.Text;
+using R2API;
+using RoR2;
+using RoR2.UI;
+using UnityEngine;
+using UnityEngine.Networking;
+using TILER2;
+using static TILER2.StatHooks;
+using static TILER2.MiscUtil;
+using System;
+
+namespace RiskOfBulletstorm.Items
+{
+    public class CultistPassiveItem : Item_V2<CultistPassiveItem>
+    {
+        public override string displayName => "Number 2";
+        public override ItemTier itemTier => ItemTier.Tier3;
+        public override ReadOnlyCollection<ItemTag> itemTags => new ReadOnlyCollection<ItemTag>(new[] { ItemTag.Damage, ItemTag.Utility, ItemTag.AIBlacklist });
+
+        protected override string GetNameString(string langID = null) => displayName;
+        protected override string GetPickupString(string langID = null) => "";
+
+        protected override string GetDescString(string langid = null) => $"";
+
+        protected override string GetLoreString(string langID = null) => "";
+
+        public override void SetupBehavior()
+        {
+
+        }
+        public override void SetupAttributes()
+        {
+            base.SetupAttributes();
+
+        }
+        public override void SetupConfig()
+        {
+            base.SetupConfig();
+        }
+        public override void Install()
+        {
+            base.Install();
+        }
+
+        public override void Uninstall()
+        {
+            base.Uninstall();
+        }
+    }
+}
