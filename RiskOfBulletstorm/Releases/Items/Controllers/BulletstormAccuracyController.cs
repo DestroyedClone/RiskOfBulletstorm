ae31d735736fa53ae843056b9e4f309a6fee070d
DestroyedClone <DestroyedClone@gmail.com>
2020-12-01 22:41:23 -0800

    Added a Controller for stats like bullet spread

---
+++ RiskOfBulletstorm/Releases/Items/Controllers/BulletstormAccuracyController.cs
@@ -0,0 +1,59 @@
+﻿//using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+//using System.Text;
+using R2API;
+using RoR2;
+using RoR2.UI;
+using UnityEngine;
+using UnityEngine.Networking;
+using TILER2;
+using static TILER2.StatHooks;
+using static TILER2.MiscUtil;
+using System;
+using static R2API.DirectorAPI;
+using static RiskOfBulletstorm.Utils.HelperUtil;
+
+namespace RiskOfBulletstorm.Items
+{
+    public class BulletstormAccuracyController : Item_V2<BulletstormAccuracyController>
+    {
+        public override string displayName => "BulletstormAccuracyController";
+        public override ItemTier itemTier => ItemTier.NoTier;
+        public override ReadOnlyCollection<ItemTag> itemTags => new ReadOnlyCollection<ItemTag>(new[] { ItemTag.WorldUnique, ItemTag.AIBlacklist });
+
+        protected override string GetNameString(string langID = null) => displayName;
+        protected override string GetPickupString(string langID = null) => "";
+
+        protected override string GetDescString(string langid = null) => $"";
+
+        protected override string GetLoreString(string langID = null) => "";
+
+        public override void SetupBehavior()
+        {
+            base.SetupBehavior();
+        }
+        public override void SetupAttributes()
+        {
+            base.SetupAttributes();
+        }
+        public override void SetupLate()
+        {
+            base.SetupLate();
+        }
+
+        public override void SetupConfig()
+        {
+            base.SetupConfig();
+        }
+        public override void Install()
+        {
+            base.Install();
+        }
+
+        public override void Uninstall()
+        {
+            base.Uninstall();
+        }
+    }
+}
