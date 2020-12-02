6357b2c07d8ffb56bebd5524195c3f8b7e1b0962
DestroyedClone <DestroyedClone@gmail.com>
2020-12-02 01:27:56 -0800

    tooltip fix

--- RiskOfBulletstorm/Releases/Items/T3/RingUnity.cs
+++ RiskOfBulletstorm/Releases/Items/T3/RingUnity.cs
@@ -35,8 +35,8 @@ namespace RiskOfBulletstorm.Items
 
         protected override string GetPickupString(string langID = null) => "Our Powers Combined\nIncreased combat effectiveness per item.";
 
-        protected override string GetDescString(string langid = null) => $"<style=cIsDamage>+{RingUnity_DamageBonus} damage</style> per unique item in inventory" +
-            $"\n<style=cStack>(+{RingUnity_DamageBonusStack} per stack)</style>";
+        protected override string GetDescString(string langid = null) => $"<style=cIsDamage>+{RingUnity_DamageBonus}base damage</style> per unique item in inventory" +
+            $"\n<style=cStack>(+{RingUnity_DamageBonusStack}base per stack)</style>";
 
         protected override string GetLoreString(string langID = null) => "This ring takes a small amount of power from each gun carried and adds it to the currently equipped gun.";
 
