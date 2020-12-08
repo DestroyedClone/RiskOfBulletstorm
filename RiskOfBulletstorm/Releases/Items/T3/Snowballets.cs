343baa932f217d6c4a59208abfffeed6a8440d8f
DestroyedClone <DestroyedClone@gmail.com>
2020-12-08 11:49:01 -0800

    Added SnowBallets

---
+++ RiskOfBulletstorm/Releases/Items/T3/Snowballets.cs
@@ -0,0 +1,53 @@
+﻿//using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+//using System.Text;
+using R2API;
+using RoR2;
+using RoR2.UI;
+using UnityEngine;
+using UnityEngine.Networking;
+using TILER2;
+using static TILER2.StatHooks;
+using static TILER2.MiscUtil;
+using System;
+
+namespace RiskOfBulletstorm.Items
+{
+    public class Snowballets : Item_V2<Snowballets>
+    {
+        public override string displayName => "";
+        public override ItemTier itemTier => ItemTier.Lunar;
+        public override ReadOnlyCollection<ItemTag> itemTags => new ReadOnlyCollection<ItemTag>(new[] { ItemTag.Damage });
+
+        protected override string GetNameString(string langID = null) => displayName;
+        protected override string GetPickupString(string langID = null) => "";
+
+        protected override string GetDescString(string langid = null) => $"";
+
+        protected override string GetLoreString(string langID = null) => "";
+
+        public override void SetupBehavior()
+        {
+            base.SetupBehavior();
+        }
+        public override void SetupAttributes()
+        {
+            base.SetupAttributes();
+
+        }
+        public override void SetupConfig()
+        {
+            base.SetupConfig();
+        }
+        public override void Install()
+        {
+            base.Install();
+        }
+
+        public override void Uninstall()
+        {
+            base.Uninstall();
+        }
+    }
+}
