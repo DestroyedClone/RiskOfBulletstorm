e5c4ed7316657f3a4713ed5fb6ade567cd8a38ef
DestroyedClone <DestroyedClone@gmail.com>
2020-12-17 19:24:44 -0800

    added a backpack

---
+++ RiskOfBulletstorm/Releases/Items/T3/Backpack.cs
@@ -0,0 +1,150 @@
+﻿
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using R2API;
+using RoR2;
+using RoR2.UI;
+using Unity;
+using UnityEngine;
+using UnityEngine.Networking;
+using TILER2;
+using BepInEx;
+using static TILER2.StatHooks;
+using static TILER2.MiscUtil;
+using System;
+
+namespace RiskOfBulletstorm.Items
+{
+    public class Backpack : Item_V2<Backpack>
+    {
+        [AutoConfigUpdateActions(AutoConfigUpdateActionTypes.InvalidateLanguage)]
+        [AutoConfig("What button should be held down to choose which slot to select?", AutoConfigFlags.None)]
+        public KeyCode Backpack_ModifierButton { get; private set; } = KeyCode.LeftShift;
+        //https://docs.unity3d.com/ScriptReference/KeyCode.html
+        public override string displayName => "Backpack";
+        public override ItemTier itemTier => ItemTier.Tier3;
+        public override ReadOnlyCollection<ItemTag> itemTags => new ReadOnlyCollection<ItemTag>(new[] { ItemTag.Utility, ItemTag.EquipmentRelated, ItemTag.AIBlacklist });
+
+        protected override string GetNameString(string langID = null) => displayName;
+        protected override string GetPickupString(string langID = null) => "Item Capacity Up!\nThe Backpack grants you the use of another Active Item. Useful, but cumbersome.";
+
+        protected override string GetDescString(string langid = null) => $"Grants one extra equipment slot." +
+            $"\nUse {Backpack_ModifierButton} and number keys to swap between slots." +
+            $"\n<link=\"youtube.com/destroyedclone\">Test</link>";
+
+        protected override string GetLoreString(string langID = null) => "";
+
+        public override void SetupBehavior()
+        {
+            base.SetupBehavior();
+        }
+        public override void SetupAttributes()
+        {
+            base.SetupAttributes();
+
+        }
+        public override void SetupConfig()
+        {
+            base.SetupConfig();
+        }
+
+
+        public override void Install()
+        {
+            base.Install();
+            On.RoR2.CharacterBody.Start += CharacterBody_Start;
+        }
+
+        public override void Uninstall()
+        {
+            base.Uninstall();
+            On.RoR2.CharacterBody.Start -= CharacterBody_Start;
+        }
+        private void CharacterBody_Start(On.RoR2.CharacterBody.orig_Start orig, CharacterBody self)
+        {
+            orig(self);
+            if (NetworkServer.active && self.isPlayerControlled)
+            {
+                var masterObj = self.masterObject;
+                BackpackComponent backpackComponent = masterObj.GetComponent<BackpackComponent>();
+                if (!backpackComponent) { backpackComponent = masterObj.AddComponent<BackpackComponent>(); }
+                backpackComponent.characterBody = self;
+            }
+        }
+
+
+        public class BackpackComponent : MonoBehaviour
+        {
+            LocalUser localUser;
+            public CharacterBody characterBody;
+            ///public Inventory inventory;
+            public byte equipmentCount = 1;
+            //private bool isToolbot = false;
+            //private byte defaultMax = 0;
+
+            public void OnEnable()
+            {
+                //isToolbot = characterBody.baseNameToken == "TOOLBOT_BODY_NAME";
+                //if (isToolbot) defaultMax = 1;
+                //inventory = characterBody.inventory;
+                localUser = LocalUserManager.readOnlyLocalUsersList[0];
+                characterBody.onInventoryChanged += CharacterBody_onInventoryChanged;
+            }
+
+            private void CharacterBody_onInventoryChanged()
+            {
+                
+            }
+
+            public void OnDisable()
+            {
+                characterBody.onInventoryChanged -= CharacterBody_onInventoryChanged;
+            }
+
+            public void Update()
+            {
+                if (!localUser.isUIFocused) //TY KingEnderBrine for the help
+                {
+                    var inventory = characterBody.inventory;
+                    if (inventory && inventory.GetItemCount(instance.catalogIndex) > 0)
+                    {
+                        if (Input.GetKey(instance.Backpack_ModifierButton))
+                        {
+                            if (Input.GetKeyDown(KeyCode.Alpha1)) SetEquipmentSlot(inventory, 0);
+                            else if (Input.GetKeyDown(KeyCode.Alpha2)) SetEquipmentSlot(inventory, 1);
+                            else if (Input.GetKeyDown(KeyCode.Alpha3)) SetEquipmentSlot(inventory, 2);
+                            else if (Input.GetKeyDown(KeyCode.Alpha4)) SetEquipmentSlot(inventory, 3);
+                            else if (Input.GetKeyDown(KeyCode.Alpha5)) SetEquipmentSlot(inventory, 4);
+                            else if (Input.GetKeyDown(KeyCode.Alpha6)) SetEquipmentSlot(inventory, 5);
+                            else if (Input.GetKeyDown(KeyCode.Alpha7)) SetEquipmentSlot(inventory, 6);
+                            else if (Input.GetKeyDown(KeyCode.Alpha8)) SetEquipmentSlot(inventory, 7);
+                            else if (Input.GetKeyDown(KeyCode.Alpha9)) SetEquipmentSlot(inventory, 8);
+                            else if (Input.GetKeyDown(KeyCode.Alpha0)) SetEquipmentSlot(inventory, 9);
+                            else if (Input.GetKeyDown(KeyCode.Minus))
+                            {
+                                var equipmentStateSlots = inventory.equipmentStateSlots;
+                                for (int i = 0; i < equipmentStateSlots.Length - 1; i++)
+                                {
+                                    Chat.AddMessage("Backpack: Slot " + i + "currently has equipment index " + equipmentStateSlots[i].equipmentIndex);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            private void SetEquipmentSlot(Inventory inventory, byte i)
+            {
+                var equipmentCount = inventory.GetEquipmentSlotCount();
+                //var value = (byte)Mathf.Min(i, equipmentCount);
+                if (i > equipmentCount)
+                {
+                    Chat.AddMessage("Backpack: Selected slot "+i+" is greater than "+ equipmentCount);
+                    return;
+                }
+                Chat.AddMessage("Backpack: Set equipment slot to " + i);
+                inventory.SetActiveEquipmentSlot(i);
+            }
+        }
+    }
+}
