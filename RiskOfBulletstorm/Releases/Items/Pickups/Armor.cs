9ec9ce48c1b1bd5e7ee6ad48c67536722b6f7979
DestroyedClone <DestroyedClone@gmail.com>
2021-02-15 12:06:02 -0800

    lunargolem idr setup

--- RiskOfBulletstorm/Releases/Items/Pickups/Armor.cs
+++ RiskOfBulletstorm/Releases/Items/Pickups/Armor.cs
@@ -273,7 +273,7 @@ localScale = new Vector3(0.033F, 0.033F, 0.033F)
             {
                 ruleType = ItemDisplayRuleType.ParentedPrefab,
                 followerPrefab = ItemBodyModelPrefab,
-                childName = "Chest",
+                childName = "Root",
                 localPos = new Vector3(0.4957F, 0.1282F, 0.3521F),
                 localAngles = new Vector3(330.3268F, 318.3562F, 295.7811F),
                 localScale = new Vector3(0.1F, 0.1F, 0.1F)
--- RiskOfBulletstorm/Releases/Items/T2/EnragingPhoto.cs
+++ RiskOfBulletstorm/Releases/Items/T2/EnragingPhoto.cs
@@ -282,7 +282,7 @@ localScale = new Vector3(2.2747F, 2.2747F, 2.2747F)
             {
                 ruleType = ItemDisplayRuleType.ParentedPrefab,
                 followerPrefab = ItemBodyModelPrefab,
-                childName = "Chest",
+                childName = "Root",
                 localPos = new Vector3(0.4957F, 0.1282F, 0.3521F),
                 localAngles = new Vector3(330.3268F, 318.3562F, 295.7811F),
                 localScale = new Vector3(0.1F, 0.1F, 0.1F)
