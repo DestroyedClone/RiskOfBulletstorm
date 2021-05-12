a0ddd564e9bdf4ec613380a57653f857c697fe1c
DestroyedClone <DestroyedClone@gmail.com>
2021-05-11 20:50:54 -0700

    s

---
+++ RiskOfBulletstorm/Releases/Items/Controllers/NewBulletstormExtraStatsController.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RiskOfBulletstorm.Releases.Items.Controllers
+{
+    class NewBulletstormExtraStatsController
+    {
+    }
+}
