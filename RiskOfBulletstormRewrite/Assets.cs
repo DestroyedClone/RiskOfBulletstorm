a34cfcb1a5fc8860c363737a7cd9f19ed5c9f276
DestroyedClone <destroyedclone@gmail.com>
2021-11-27 19:50:26 -0800

    code taken from anreol

---
+++ RiskOfBulletstormRewrite/Assets.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace RiskOfBulletstormRewrite
+{
+    class Assets //Meant to be used to hold assetbundle shit... this mod does not use that, though, but still
+    {
+        internal static string assemblyDir
+        {
+            get
+            {
+                return Path.GetDirectoryName(Main.pluginInfo.Location);
+            }
+        }
+    }
+}
