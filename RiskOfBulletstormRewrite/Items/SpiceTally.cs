5bac65a191a97bd5caf0d6ccdfd05a1346fd703b
DestroyedClone <destroyedclone@gmail.com>
2021-11-27 19:49:42 -0800

    Updated SpiceTally to latest ItemBase

--- RiskOfBulletstormRewrite/Items/SpiceTally.cs
+++ RiskOfBulletstormRewrite/Items/SpiceTally.cs
@@ -1,31 +1,22 @@
 ﻿using BepInEx.Configuration;
 using R2API;
-using RiskOfBulletstormRewrite.Utils;
 using RoR2;
 using UnityEngine;
 using static RiskOfBulletstormRewrite.Main;
-using static RiskOfBulletstormRewrite.Utils.ItemHelpers;
 
 namespace RiskOfBulletstormRewrite.Items
 {
     public class SpiceTally : ItemBase<SpiceTally>
     {
-
         public override string ItemName => "Spice (Consumed)";
 
         public override string ItemLangTokenName => "SPICETALLY";
-
-        public override string ItemPickupDesc => "It's delicious!";
-
-        public override string ItemFullDescription => $"Grants various stat changes. Most notably, increased damage and reduced accuracy and health.";
-
-        public override string ItemLore => "";
 
         public override ItemTier Tier => ItemTier.NoTier;
 
         public override GameObject ItemModel => MainAssets.LoadAsset<GameObject>("Assets/Models/Prefabs/Scope.prefab");
 
-        public override Sprite ItemIcon => MainAssets.LoadAsset<Sprite>("Assets/Textures/Icons/Scope.png");
+        public override Sprite ItemIcon => MainAssets.LoadAsset<Sprite>("Assets/Textures/Icons/Spice.png");
 
         public static GameObject ItemBodyModelPrefab;
 
@@ -34,17 +25,11 @@ namespace RiskOfBulletstormRewrite.Items
             CreateConfig(config);
             CreateLang();
             CreateItem();
-            Hooks();
         }
 
         public override ItemDisplayRuleDict CreateItemDisplayRules()
         {
             return new ItemDisplayRuleDict();
-        }
-
-        public override void Hooks()
-        {
-
         }
     }
 }
