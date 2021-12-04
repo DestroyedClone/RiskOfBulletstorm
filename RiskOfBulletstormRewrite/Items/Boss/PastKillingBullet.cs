543cac748264b2ece43c77cab5a275340e44fa36
DestroyedClone <destroyedclone@gmail.com>
2021-12-04 15:02:24 -0800

    ported past killing bullet

---
+++ RiskOfBulletstormRewrite/Items/Boss/PastKillingBullet.cs
@@ -0,0 +1,62 @@
+﻿using BepInEx.Configuration;
+using R2API;
+using RoR2;
+using UnityEngine;
+using static RiskOfBulletstormRewrite.Main;
+
+namespace RiskOfBulletstormRewrite.Items
+{
+    public class PastKillingBullet : ItemBase<PastKillingBullet>
+    {
+        public override string ItemName => "Past Killing Bullet";
+
+        public override string ItemLangTokenName => "PASTKILLINGBULLET";
+
+        public override ItemTier Tier => ItemTier.Boss;
+
+        public override GameObject ItemModel => null;
+
+        public override Sprite ItemIcon => null;
+
+        public override ItemTag[] ItemTags => new ItemTag[]
+        {
+            ItemTag.Any,
+            ItemTag.AIBlacklist,
+            ItemTag.BrotherBlacklist,
+            ItemTag.CannotCopy,
+            ItemTag.CannotSteal,
+            ItemTag.WorldUnique
+        };
+
+        public override void Init(ConfigFile config)
+        {
+            CreateConfig(config);
+            CreateLang();
+            CreateItem();
+            Hooks();
+        }
+
+        public override void CreateConfig(ConfigFile config)
+        {
+
+        }
+
+        public override ItemDisplayRuleDict CreateItemDisplayRules()
+        {
+            return new ItemDisplayRuleDict();
+        }
+
+        public override void Hooks()
+        {
+            On.RoR2.GenericPickupController.OnTriggerStay += PreventBulletAutoPickup;
+        }
+
+        private void PreventBulletAutoPickup(On.RoR2.GenericPickupController.orig_OnTriggerStay orig, GenericPickupController self, Collider other)
+        {
+            if (self.pickupIndex != PickupCatalog.FindPickupIndex(PastKillingBullet.instance.ItemDef.itemIndex))
+                orig(self, other);
+            else
+                orig(self, null);
+        }
+    }
+}
