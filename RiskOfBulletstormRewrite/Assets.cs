87ae661552eca9a2edf95df626980208f4c140f0
DestroyedClone <destroyedclone@gmail.com>
2021-12-26 01:22:56 -0800

    Added static references to the default null objects

--- RiskOfBulletstormRewrite/Assets.cs
+++ RiskOfBulletstormRewrite/Assets.cs
@@ -2,11 +2,16 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using UnityEngine;
 
 namespace RiskOfBulletstormRewrite
 {
     class Assets
     {
+        internal static GameObject NullModel = Resources.Load<GameObject>("prefabs/nullmodel");
+        internal static Sprite NullSprite = Resources.Load<Sprite>("textures/itemicons/texnullicon");
+
+
         internal static string assemblyDir
         {
             get
