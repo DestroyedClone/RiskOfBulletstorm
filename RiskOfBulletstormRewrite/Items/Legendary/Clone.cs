c9d7d05e6b40d55916b21678fca7f754da447f80
DestroyedClone <destroyedclone@gmail.com>
2022-01-02 00:59:54 -0800

    Added Clone

---
+++ RiskOfBulletstormRewrite/Items/Legendary/Clone.cs
@@ -0,0 +1,110 @@
+﻿using BepInEx.Configuration;
+using R2API;
+using RoR2;
+using UnityEngine;
+using static RiskOfBulletstormRewrite.Main;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using System.Collections.ObjectModel;
+
+namespace RiskOfBulletstormRewrite.Items
+{
+    public class Clone : ItemBase<Clone>
+    {
+        public override string ItemName => "Clone";
+
+        public override string ItemLangTokenName => "CLONE";
+
+        public override ItemTier Tier => ItemTier.Tier3;
+
+        public override GameObject ItemModel => Assets.NullModel;
+
+        public override Sprite ItemIcon => Assets.NullSprite;
+
+        public override ItemTag[] ItemTags => new ItemTag[]
+        {
+            ItemTag.AIBlacklist
+        };
+
+        public static bool isCloneRestarting = false;
+
+        public override void Init(ConfigFile config)
+        {
+            CreateConfig(config);
+            CreateLang();
+            CreateItem();
+            Hooks();
+        }
+
+        public override void CreateConfig(ConfigFile config)
+        {
+
+        }
+
+        public override ItemDisplayRuleDict CreateItemDisplayRules()
+        {
+            return new ItemDisplayRuleDict();
+        }
+
+        public override void Hooks()
+        {
+            On.RoR2.PlayerCharacterMasterController.OnBodyDeath += PlayerCharacterMasterController_OnBodyDeath;
+            On.RoR2.CharacterMaster.SpawnBody += CharacterMaster_SpawnBody;
+            On.RoR2.Run.BeginGameOver += PreventGameOverIfClone;
+        }
+
+        private CharacterBody CharacterMaster_SpawnBody(On.RoR2.CharacterMaster.orig_SpawnBody orig, CharacterMaster self, Vector3 position, Quaternion rotation)
+        {
+            isCloneRestarting = false;
+            return orig(self, position, rotation);
+        }
+
+        private void PreventGameOverIfClone(On.RoR2.Run.orig_BeginGameOver orig, Run self, GameEndingDef gameEndingDef)
+        {
+            if (!isCloneRestarting)
+                orig(self, gameEndingDef);
+        }
+
+        private void PlayerCharacterMasterController_OnBodyDeath(On.RoR2.PlayerCharacterMasterController.orig_OnBodyDeath orig, PlayerCharacterMasterController self)
+        {
+            orig(self);
+            if (!NetworkServer.active) return;
+            var stage = RoR2.Stage.instance;
+            if (stage && stage.spawnedAnyPlayer && stage.stageAdvanceTime.isInfinity && !Run.instance.isGameOverServer)
+            {
+                ReadOnlyCollection<PlayerCharacterMasterController> instances = PlayerCharacterMasterController.instances;
+                bool allDead = true;
+                List<CharacterMaster> peopleWithClones = new List<CharacterMaster>();
+                for (int i = 0; i < instances.Count; i++)
+                {
+                    PlayerCharacterMasterController playerCharacterMasterController = instances[i];
+                    if (playerCharacterMasterController.isConnected)
+                    {
+                        if (!playerCharacterMasterController.master.IsDeadAndOutOfLivesServer())
+                        {
+                            // If there's a player alive, end the loop early.
+                            allDead = false;
+                            break;
+                        } else
+                        {
+                            if (GetCount(playerCharacterMasterController.master) > 0)
+                            {
+                                peopleWithClones.Add(playerCharacterMasterController.master);
+                            }
+                        }
+                    }
+                }
+                if (allDead && peopleWithClones.Count > 0)
+                {
+                    peopleWithClones[0].inventory.RemoveItem(ItemDef);
+                    isCloneRestarting = true;
+                    Run.instance.SetRunStopwatch(0);
+                    TeamManager.instance.SetTeamLevel(TeamIndex.Monster, 1);
+                    SceneDef[] choices = Run.instance.startingScenes;
+                    Run.instance.PickNextStageScene(choices);
+                    Run.instance.AdvanceStage(Run.instance.nextStageScene);
+                }
+            }
+        }
+    }
+}
