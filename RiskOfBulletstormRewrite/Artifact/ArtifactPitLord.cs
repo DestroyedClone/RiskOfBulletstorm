d37166d9934927f5353431262bee322a2097f448
DestroyedClone <destroyedclone@gmail.com>
2022-04-09 01:19:12 -0700

    Removed PlayerOnly method due to change

--- RiskOfBulletstormRewrite/Artifact/ArtifactPitLord.cs
+++ RiskOfBulletstormRewrite/Artifact/ArtifactPitLord.cs
@@ -1,15 +1,11 @@
 ﻿using BepInEx.Configuration;
 using RoR2;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using UnityEngine;
-using UnityEngine.Networking;
 using static RiskOfBulletstormRewrite.Main;
 
 namespace RiskOfBulletstormRewrite.Artifact
 {
-    class ArtifactPitLord : ArtifactBase<ArtifactPitLord>
+    internal class ArtifactPitLord : ArtifactBase<ArtifactPitLord>
     {
         public static ConfigEntry<bool> cfgAffectMonsters;
 
@@ -61,23 +57,6 @@ namespace RiskOfBulletstormRewrite.Artifact
         }
 
         //https://github.com/KomradeSpectre/AetheriumMod/blob/rewrite-master/Aetherium/Items/UnstableDesign.cs#L505-L520
-        public static void PlayerNoFallDamage(On.RoR2.MapZone.orig_TryZoneStart orig, MapZone self, Collider other)
-        {
-            CharacterBody body = other.GetComponent<CharacterBody>();
-            if (body && body.teamComponent && body.teamComponent.teamIndex == TeamIndex.Player)
-            {
-                //GEM.Oncharacterhitground
-                if (CharacterBodyCanTakeFallDamage(body))
-                {
-                    body.bodyFlags &= CharacterBody.BodyFlags.IgnoreFallDamage;
-                    orig(self, other);
-                    body.bodyFlags |= CharacterBody.BodyFlags.IgnoreFallDamage;
-                    return;
-                }
-            }
-            orig(self, other);
-        }
-
         private void AllNoFallDamage(On.RoR2.MapZone.orig_TryZoneStart orig, MapZone self, Collider other)
         {
             CharacterBody body = other.GetComponent<CharacterBody>();
@@ -91,7 +70,7 @@ namespace RiskOfBulletstormRewrite.Artifact
                 if (canTakeFallDamage) body.bodyFlags &= CharacterBody.BodyFlags.IgnoreFallDamage;
 
                 orig(self, other); //Run the effect of whatever zone it is in on it. Since it is of the Player team, it obviously gets teleported back into the zone.
-                
+
                 teamComponent.teamIndex = oldTeamIndex; //Now make it hostile again. Thanks Obama.
                 if (canTakeFallDamage) body.bodyFlags |= CharacterBody.BodyFlags.IgnoreFallDamage;
                 return;
