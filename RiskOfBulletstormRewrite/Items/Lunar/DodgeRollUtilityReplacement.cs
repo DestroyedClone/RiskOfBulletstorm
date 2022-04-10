e322c9a68e64a1496a580fc195475315ee9fbc61
DestroyedClone <destroyedclone@gmail.com>
2022-04-09 21:28:50 -0700

    Added Dodge Roll

---
+++ RiskOfBulletstormRewrite/Items/Lunar/DodgeRollUtilityReplacement.cs
@@ -0,0 +1,98 @@
+﻿using BepInEx.Configuration;
+using R2API;
+using RoR2;
+using UnityEngine;
+using UnityEngine.Networking;
+using static RiskOfBulletstormRewrite.Main;
+using RoR2.Skills;
+using EntityStates;
+
+namespace RiskOfBulletstormRewrite.Items
+{
+    public class DodgeRollUtilityReplacement : ItemBase<DodgeRollUtilityReplacement>
+    {
+        public override string ItemName => "DodgeRollUtilityReplacement";
+
+        public override string ItemLangTokenName => "DODGEROLLUTILITYREPLACEMENT";
+
+        public override ItemTier Tier => ItemTier.Lunar;
+
+        public override GameObject ItemModel => Assets.NullModel;
+
+        public override Sprite ItemIcon => Assets.NullSprite;
+
+        public override ItemTag[] ItemTags => new ItemTag[]
+        {
+            ItemTag.Cleansable,
+            ItemTag.Utility
+        };
+
+        public static SkillDef rollSkillDef;
+
+        public override void Init(ConfigFile config)
+        {
+            CreateConfig(config);
+            CreateLang();
+            CreateItem();
+            Hooks();
+            CreateSkillDef();
+        }
+
+        public void CreateSkillDef()
+        {
+            rollSkillDef = ScriptableObject.CreateInstance<SkillDef>();
+            rollSkillDef.activationState = new SerializableEntityStateType(typeof(EntityStates.Commando.CombatDodge));
+            rollSkillDef.activationStateMachineName = "Body";
+            rollSkillDef.baseMaxStock = 1;
+            rollSkillDef.baseRechargeInterval = 12;
+            rollSkillDef.beginSkillCooldownOnSkillEnd = true;
+            rollSkillDef.canceledFromSprinting = false;
+            rollSkillDef.cancelSprintingOnActivation = false;
+            rollSkillDef.dontAllowPastMaxStocks = false;
+            rollSkillDef.forceSprintDuringState = true;
+            rollSkillDef.fullRestockOnAssign = true;
+            rollSkillDef.icon = Assets.NullSprite;
+            rollSkillDef.interruptPriority = InterruptPriority.Pain;
+            rollSkillDef.isCombatSkill = false;
+            rollSkillDef.keywordTokens = new string[]
+            {
+                "KEYWORD_AGILE"
+            };
+            rollSkillDef.mustKeyPress = true;
+            rollSkillDef.rechargeStock = 1;
+            rollSkillDef.requiredStock = 1;
+            rollSkillDef.resetCooldownTimerOnUse = true;
+            rollSkillDef.skillDescriptionToken = "RISKOFBULLETSTORM_SKILL_TELEPORT_DESCRIPTION";
+            rollSkillDef.skillName = "RiskOfBulletstormTeleport";
+            rollSkillDef.skillNameToken = "RISKOFBULLETSTORM_SKILL_TELEPORT_NAME";
+            (rollSkillDef as ScriptableObject).name = "RiskOfBulletstormTeleport";
+            rollSkillDef.stockToConsume = 1;
+
+            ContentAddition.AddSkillDef(rollSkillDef);
+        }
+
+        public override void CreateConfig(ConfigFile config)
+        {
+
+        }
+
+        public override ItemDisplayRuleDict CreateItemDisplayRules()
+        {
+            return new ItemDisplayRuleDict();
+        }
+
+        public override void Hooks()
+        {
+            CharacterBody.onBodyInventoryChangedGlobal += CharacterBody_onBodyInventoryChangedGlobal;
+        }
+
+        private void CharacterBody_onBodyInventoryChangedGlobal(CharacterBody characterBody)
+        {
+            if (characterBody.skillLocator)
+            {
+                characterBody.ReplaceSkillIfItemPresent(characterBody.skillLocator.utility, ItemDef.itemIndex, rollSkillDef);
+            }
+        }
+
+    }
+}
