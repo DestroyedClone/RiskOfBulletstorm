b8db5d8cd956bb50e4470f0236cfb15e5071c3e1
DestroyedClone <destroyedclone@gmail.com>
2022-04-23 09:02:24 -0700

    Added Iron Coin

---
+++ RiskOfBulletstormRewrite/Equipment/Lunar/IronCoin.cs
@@ -0,0 +1,78 @@
+﻿using BepInEx.Configuration;
+using R2API;
+using RoR2;
+using UnityEngine;
+using static RiskOfBulletstormRewrite.Main;
+
+namespace RiskOfBulletstormRewrite.Equipment
+{
+    public class IronCoin : EquipmentBase<IronCoin>
+    {
+        //public static ConfigEntry<float> cfg;
+
+        public override string EquipmentName => "Iron Coin";
+
+        public override string EquipmentLangTokenName => "IRONCOIN";
+
+        public override GameObject EquipmentModel => Assets.NullModel;
+
+        public override Sprite EquipmentIcon => Assets.NullSprite;
+
+        public override void Init(ConfigFile config)
+        {
+            CreateConfig(config);
+            CreateLang();
+            CreateEquipment();
+            Hooks();
+        }
+
+        protected override void CreateConfig(ConfigFile config)
+        {
+
+        }
+
+        public override ItemDisplayRuleDict CreateItemDisplayRules()
+        {
+            return new ItemDisplayRuleDict();
+        }
+
+        protected override bool ActivateEquipment(EquipmentSlot slot)
+        {
+            int enemiesKilled = 0;
+            var attackerGameObject = slot.characterBody.gameObject;
+            foreach (var charMaster in CharacterMaster.instancesList)
+            {
+                var body = charMaster.GetBody();
+                if (charMaster.inventory && body && body.healthComponent && body.healthComponent.alive && charMaster.teamIndex != slot.teamComponent.teamIndex)
+                {
+                    enemiesKilled++;
+
+                    if (body.TryGetComponent<DeathRewards>(out DeathRewards deathRewards))
+                    {
+                        deathRewards.goldReward = 0;
+                        deathRewards.expReward = 0;
+                        deathRewards.bossDropTable = null;
+                    }
+                    if (body.isElite && body.TryGetComponent(out EquipmentSlot equipmentSlot))
+                    {
+                        equipmentSlot.equipmentIndex = EquipmentIndex.None;
+                    }
+                    var itemCount = charMaster.inventory.GetItemCount(RoR2Content.Items.InvadingDoppelganger);
+                    if (itemCount > 0)
+                    {
+                        charMaster.inventory.RemoveItem(RoR2Content.Items.InvadingDoppelganger, itemCount);
+                    }
+                    charMaster.TrueKill(attackerGameObject, attackerGameObject, DamageType.Generic);
+                }
+            }
+            if (enemiesKilled > 0)
+            {
+                slot.equipmentIndex = EquipmentIndex.None;
+                return true;
+            }
+            return false;
+        }
+
+
+    }
+}
