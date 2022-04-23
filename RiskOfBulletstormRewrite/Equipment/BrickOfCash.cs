253105a5a3962d01a53e07935a3013e0f80300aa
DestroyedClone <destroyedclone@gmail.com>
2022-04-23 03:58:51 -0700

    Added more incomplete functionality for Brick of Cash

--- RiskOfBulletstormRewrite/Equipment/BrickOfCash.cs
+++ RiskOfBulletstormRewrite/Equipment/BrickOfCash.cs
@@ -4,6 +4,7 @@ using RoR2;
 using UnityEngine;
 using static RiskOfBulletstormRewrite.Main;
 using UnityEngine.Networking;
+using System.Collections.Generic;
 
 namespace RiskOfBulletstormRewrite.Equipment
 {
@@ -38,38 +39,113 @@ namespace RiskOfBulletstormRewrite.Equipment
         {
             On.RoR2.ChestBehavior.Start += ChestBehavior_Start;
             On.RoR2.PressurePlateController.Start += PressurePlateController_Start;
+            On.RoR2.Inventory.SetEquipmentInternal += Inventory_SetEquipmentInternal;
 
+            //instance tracking
+            On.RoR2.ChestBehavior.Start += AddChestBehavior;
+            On.RoR2.PressurePlateController.Start += AddPressurePlateController;
+        }
+
+        private void AddPressurePlateController(On.RoR2.PressurePlateController.orig_Start orig, PressurePlateController self)
+        {
+            orig(self);
+            InstanceTracker.Add(self);
+            if (!self.GetComponent<BulletstormInstanceTrackerRemover>())
+                self.gameObject.AddComponent<BulletstormInstanceTrackerRemover>().pressurePlateController = self;
+        }
+
+        private void AddChestBehavior(On.RoR2.ChestBehavior.orig_Start orig, ChestBehavior self)
+        {
+            orig(self);
+            InstanceTracker.Add(self);
+            if (!self.GetComponent<BulletstormInstanceTrackerRemover>())
+                self.gameObject.AddComponent<BulletstormInstanceTrackerRemover>().chestBehavior = self;
+        }
+
+        public class BulletstormInstanceTrackerRemover : MonoBehaviour
+        {
+            public ChestBehavior chestBehavior;
+            public PressurePlateController pressurePlateController;
+
+            public void OnDestroy()
+            {
+                if (chestBehavior)
+                    InstanceTracker.Remove(chestBehavior);
+                if (pressurePlateController)
+                    InstanceTracker.Remove(pressurePlateController);
+            }
+        }
+
+        private bool Inventory_SetEquipmentInternal(On.RoR2.Inventory.orig_SetEquipmentInternal orig, Inventory self, EquipmentState equipmentState, uint slot)
+        {
+            bool isCash = equipmentState.equipmentDef == EquipmentDef;
+            var original = orig(self, equipmentState, slot);
+            if (isCash && original)
+            {
+                RatOut();
+            }
+            return original;
+        }
+
+        private void RatOut()
+        {
+            foreach (var chestBehavior in InstanceTracker.GetInstancesList<ChestBehavior>())
+            {
+                if (ChestCheck(chestBehavior))
+                {
+                    CreateIndicator(chestBehavior.transform);
+                }
+            }
+            foreach (var pressurePlateController in InstanceTracker.GetInstancesList<PressurePlateController>())
+            {
+                CreateIndicator(pressurePlateController.transform);
+            }
         }
 
         private void PressurePlateController_Start(On.RoR2.PressurePlateController.orig_Start orig, PressurePlateController self)
         {
             orig(self);
-            if (Utils.ItemHelpers.GetAtLeastOneEquipmentForTeam(TeamIndex.Player, BrickOfCash.instance.EquipmentDef.equipmentIndex, false))
+            if (PlayersHaveBrickOfCash())
                 CreateIndicator(self.transform);
         }
 
         private void ChestBehavior_Start(On.RoR2.ChestBehavior.orig_Start orig, ChestBehavior self)
         {
             orig(self);
-            var comp = self.GetComponent<PurchaseInteraction>();
+            if (ChestCheck(self) && PlayersHaveBrickOfCash())
+            {
+                CreateIndicator(self.transform);
+            }
+        }
+
+        private bool ChestCheck(ChestBehavior chestBehavior)
+        {
+            var comp = chestBehavior.GetComponent<PurchaseInteraction>();
             if (comp)
             {
                 if (comp.displayNameToken == "CHEST1_STEALTHED_NAME")
                 {
-                    if (Utils.ItemHelpers.GetAtLeastOneEquipmentForTeam(TeamIndex.Player, BrickOfCash.instance.EquipmentDef.equipmentIndex, false))
-                        CreateIndicator(comp.transform);
+                    return true;
                 }
             }
+            return false;
+        }
+
+        private bool PlayersHaveBrickOfCash()
+        {
+            return Utils.ItemHelpers.GetAtLeastOneEquipmentForTeam(TeamIndex.Player, BrickOfCash.instance.EquipmentDef.equipmentIndex, true);
         }
 
         private void CreateIndicator(Transform parent)
         {
             if (!NetworkServer.active) return;
-            var prefab = LegacyResourcesAPI.Load<GameObject>("RoR2/Base/Common/PoiPositionIndicator");
-            var copy = Object.Instantiate(prefab, parent.position, Quaternion.identity, parent);
-            copy.GetComponent<PositionIndicator>().targetTransform = parent;
-            NetworkServer.Spawn(copy);
-            UnityEngine.Object.Destroy(copy);
+            ChestRevealer.PendingReveal item = new ChestRevealer.PendingReveal
+            {
+                gameObject = parent.gameObject,
+                time = Run.FixedTimeStamp.now,
+                duration = Mathf.Infinity
+            };
+            ChestRevealer.pendingReveals.Add(item);
         }
 
         protected override void CreateConfig(ConfigFile config)
