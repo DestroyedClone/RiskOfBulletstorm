25373c523542090667fa9608455aa9a82b7ca17a
DestroyedClone <DestroyedClone@gmail.com>
2022-10-04 21:43:59 -0700

    Added new artifact: Boss Chamber

---
+++ RiskOfBulletstormRewrite/Artifact/ArtifactBossRoom.cs
@@ -0,0 +1,71 @@
+﻿using BepInEx.Configuration;
+using RoR2;
+using UnityEngine;
+using static RiskOfBulletstormRewrite.Main;
+using UnityEngine.Networking;
+
+namespace RiskOfBulletstormRewrite.Artifact
+{
+    internal class ArtifactBossRoom : ArtifactBase<ArtifactBossRoom>
+    {
+        public override string ArtifactName => "Artifact of the Boss Chamber";
+
+        public override string ArtifactLangTokenName => "BOSSCHAMBER";
+
+        public override Sprite ArtifactEnabledIcon => Assets.NullSprite;
+
+        public override Sprite ArtifactDisabledIcon => Assets.NullSprite;
+
+        public override void Init(ConfigFile config)
+        {
+            CreateLang();
+            CreateArtifact();
+            Hooks();
+        }
+
+        public override void Hooks()
+        {
+            RunArtifactManager.onArtifactEnabledGlobal += RunArtifactManager_onArtifactEnabledGlobal;
+            RunArtifactManager.onArtifactDisabledGlobal += RunArtifactManager_onArtifactDisabledGlobal;
+        }
+
+        private void RunArtifactManager_onArtifactDisabledGlobal([JetBrains.Annotations.NotNull] RunArtifactManager runArtifactManager, [JetBrains.Annotations.NotNull] ArtifactDef artifactDef)
+        {
+            if (artifactDef != ArtifactDef)
+            {
+                return;
+            }
+            TeleporterInteraction.onTeleporterBeginChargingGlobal -= ClearSpawns;
+        }
+
+
+        private void RunArtifactManager_onArtifactEnabledGlobal([JetBrains.Annotations.NotNull] RunArtifactManager runArtifactManager, [JetBrains.Annotations.NotNull] ArtifactDef artifactDef)
+        {
+            if (artifactDef != ArtifactDef)
+            {
+                return;
+            }
+            //teleporter interaction
+            TeleporterInteraction.onTeleporterBeginChargingGlobal += ClearSpawns;
+        }
+
+        public void ClearSpawns(TeleporterInteraction self)
+        {
+            if (NetworkServer.active)
+            {
+                foreach (var enemy in CharacterMaster.readOnlyInstancesList)
+                {
+                    if (enemy.teamIndex == TeamIndex.Monster)
+                    {
+                        var body = enemy.GetBody();
+                        if (body && body.healthComponent && !body.isBoss)
+                        {
+                            body.healthComponent.Suicide();
+                        }
+                    }
+                }
+                self.bonusDirector.enabled = false;
+            }
+        }
+    }
+}
