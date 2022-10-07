e8948bda539acb4d87309c916fdc3c796f241774
DestroyedClone <DestroyedClone@gmail.com>
2022-10-07 00:20:40 -0700

    Somehow added babygoodmimic

--- RiskOfBulletstormRewrite/Items/Legendary/BabyGoodMimic.cs
+++ RiskOfBulletstormRewrite/Items/Legendary/BabyGoodMimic.cs
@@ -4,6 +4,7 @@ using RoR2;
 using UnityEngine;
 using UnityEngine.Networking;
 using static RiskOfBulletstormRewrite.Main;
+using System.Collections.Generic;
 
 namespace RiskOfBulletstormRewrite.Items
 {
@@ -45,7 +46,145 @@ namespace RiskOfBulletstormRewrite.Items
 
         public override void Hooks()
         {
+            Inventory.onServerItemGiven += ReplaceMimicItem;
+            Inventory.onInventoryChangedGlobal += OnInventoryChangedGlobal;
+        }
 
+        public void OnInventoryChangedGlobal(Inventory inventory)
+        {
+            if (NetworkServer.active)
+            {
+                var comp = inventory.GetComponent<RBS_BabyMimicBehaviour>();
+                if (inventory.GetItemCount(ItemDef) > 0)
+                {
+                    if (!comp)
+                    {
+                        comp = inventory.gameObject.AddComponent<RBS_BabyMimicBehaviour>();
+                        comp.ownerMaster = inventory.GetComponent<CharacterMaster>();
+                    }
+                } else {
+                    if (comp)
+                    {
+                        UnityEngine.Object.Destroy(comp);
+                    }
+                }
+            }
+        }
+
+        //down, right, down, up, down, up
+        //died to FUCKING rat
+        public void ReplaceMimicItem(Inventory inventory, ItemIndex itemIndex, int count)
+        {
+
+        }
+
+        public class RBS_BabyMimicBehaviour : MonoBehaviour
+        {
+            public CharacterMaster ownerMaster;
+            public CharacterBody ownerBody;
+            public Inventory inventory;
+            public MinionOwnership.MinionGroup minionGroup;
+            public float stopwatch = 0;
+            float duration = 30f;
+            int itemCount = 0;
+
+            public List<CharacterMaster> mimics = new List<CharacterMaster>();
+
+            public void Awake()
+            {
+                minionGroup = MinionOwnership.MinionGroup.FindGroup(ownerBody.master.netId);
+            }   
+
+            public void OnEnable()
+            {
+                inventory = ownerBody.inventory;
+                inventory.onInventoryChanged += UpdateItemCount;
+                ownerMaster.onBodyStart += OnOwnerBodyStart;
+            }
+
+            public void OnDisable()
+            {
+                inventory.onInventoryChanged -= UpdateItemCount;
+                ownerMaster.onBodyStart -= OnOwnerBodyStart;
+            }
+
+            public void OnOwnerBodyStart(CharacterBody body)
+            {
+                ownerBody = body;
+            }
+
+            public void UpdateItemCount()
+            {
+                itemCount = inventory.GetItemCount(instance.ItemDef);
+            }
+
+            public void FixedUpdate()
+            {
+                stopwatch += Time.fixedDeltaTime;
+                if (stopwatch >= duration)
+                {
+                    stopwatch = 0;
+                    if (ownerBody)
+                        UpdateMinions();
+                }
+            }
+
+            public void UpdateMinions()
+            {
+                int minionsToCopy = itemCount - mimics.Count;
+
+                foreach (var minion in minionGroup.members)
+                {
+                    var cb = minion.GetComponent<CharacterBody>();
+                    if (cb && cb.healthComponent && cb.healthComponent.alive
+                    && cb.inventory && cb.master 
+                    && cb.inventory.GetItemCount(RoR2Content.Items.Ghost) == 0
+                    && !cb.bodyFlags.HasFlag(CharacterBody.BodyFlags.Mechanical))
+                    {
+                        AssignMinion(cb);
+                        break;
+                    }
+                }
+            }
+
+            public void AssignMinion(CharacterBody targetMinion)
+            {
+                var copy = Util.TryToCreateGhost(targetMinion, ownerBody, 10);
+                copy.inventory.RemoveItem(RoR2Content.Items.Ghost);
+                copy.inventory.RemoveItem(RoR2Content.Items.HealthDecay, 10);
+                copy.baseNameToken = "RISKOFBULLETSTORM_BABYGOODMIMIC_BODY_NAME";
+                copy.subtitleNameToken = "RISKOFBULLETSTORM_BABYGOODMIMIC_BODY_SUBTITLE";
+                TeleportHelper.TeleportBody(copy, ownerBody.footPosition);
+
+                var comp = copy.gameObject.AddComponent<RBS_BabyMimicPairing>();
+                comp.myBody = copy;
+                comp.ownerBody = ownerBody;
+                //comp.targetBody = targetMinion;
+                comp.targetMaster = targetMinion.master;
+                
+            }
+        }
+
+        public class RBS_BabyMimicPairing : MonoBehaviour
+        {
+            public CharacterBody ownerBody;
+            public CharacterBody myBody;
+            public CharacterMaster targetMaster;
+
+            public void OnEnable()
+            {
+                targetMaster.onBodyDestroyed += OnBodyDestroyed;
+            }
+
+            public void OnDisable()
+            {
+                targetMaster.onBodyDestroyed -= OnBodyDestroyed;
+            }
+
+            public void OnBodyDestroyed(CharacterBody characterBody)
+            {
+                myBody.master.TrueKill();
+            }
         }
 
     }
