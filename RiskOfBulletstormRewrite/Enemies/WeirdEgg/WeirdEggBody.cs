5d372d5a3c210a225bb4e2b5523f2e87d8234b53
DestroyedClone <destroyedclone@gmail.com>
2023-01-26 02:57:20 -0800

    Added start of WeirdEgg stuff

---
+++ RiskOfBulletstormRewrite/Enemies/WeirdEgg/WeirdEggBody.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RiskOfBulletstormRewrite.Enemies.WeirdEgg
+{
+    internal class WeirdEggBody
+    {
+    }
+}
---
+++ RiskOfBulletstormRewrite/Enemies/WeirdEgg/WeirdEggMain.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using RoR2;
+using EntityStates;
+
+namespace RiskOfBulletstormRewrite.Enemies.WeirdEgg
+{
+    public class WeirdEggMain : GenericCharacterMain, IOnTakeDamageServerReceiver
+    {
+        public CharacterBody characterBody;
+        public int stagesCleared = 0;
+
+        void IOnTakeDamageServerReceiver.OnTakeDamageServer(DamageReport damageReport)
+        {
+            if (damageReport.damageInfo.damageType.HasFlag(DamageType.IgniteOnHit)
+                || damageReport.damageInfo.damageType.HasFlag(DamageType.PercentIgniteOnHit))
+            {
+                SpawnEel();
+            } else
+            {
+                if (CheckStageClears())
+                {
+                    SpawnItem();
+                } else
+                {
+                    SpawnSlug();
+                }
+            }
+            return;
+        }
+
+        public void SpawnSlug() { }
+        public void SpawnEel() { }
+        public void SpawnItem() { }
+
+        public bool CheckStageClears()
+        {
+            if (Run.instance)
+            {
+                return Run.instance.stageClearCount > 4;
+            }
+            return true;
+        }
+
+    }
+}
