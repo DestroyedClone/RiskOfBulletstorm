0ed1d468afc71edf0befc04b3b4f212c507c9f64
DestroyedClone <destroyedclone@gmail.com>
2023-03-22 12:27:37 -0700

    Iron Coin: Added behavior for if the player is a target Iron Coin (Consumed): Added side flip result, Added config to prevent non-players from spamming chat

--- RiskOfBulletstormRewrite/Equipment/Lunar/IronCoin.cs
+++ RiskOfBulletstormRewrite/Equipment/Lunar/IronCoin.cs
@@ -1,5 +1,6 @@
 ﻿using BepInEx.Configuration;
 using R2API;
+using RiskOfBulletstormRewrite.Utils;
 using RoR2;
 using UnityEngine;
 
@@ -7,7 +8,8 @@ namespace RiskOfBulletstormRewrite.Equipment
 {
     public class IronCoin : EquipmentBase<IronCoin>
     {
-        //public static ConfigEntry<float> cfg;
+        public static ConfigEntry<float> cfgDamageToPlayers;
+        public static ConfigEntry<IronCoinEnum> cfgEnemyPlayerDamageType;
 
         public override string EquipmentName => "Iron Coin";
 
@@ -27,13 +29,294 @@ namespace RiskOfBulletstormRewrite.Equipment
             Hooks();
         }
 
+        public enum IronCoinEnum
+        {
+            Nonfatal,
+            Fatal,
+            InstantKill
+        }
+
         protected override void CreateConfig(ConfigFile config)
         {
+            cfgDamageToPlayers = config.Bind(ConfigCategory, "Damage Dealt To Enemy Players", 0.05f, "If flipped by an enemy, deals this amount of damage to enemy players." +
+                "\nDamage dealt is equal to config value multiplied by enemy player's max health." +
+                "\nEx: 0.05 -> 5% of the enemy player's max health.");
+            cfgEnemyPlayerDamageType = config.Bind(ConfigCategory, "Damage Type Dealt To Players",
+                IronCoinEnum.Nonfatal,
+                "Nonfatal - Deals damage based off the other config value, but can not kill the player" +
+                "\n Fatal - Damage dealt can kill the player" +
+                "\n  InstantKill - Damage dealt instantly kills the player, ignores damage dealt.");
         }
 
         public override ItemDisplayRuleDict CreateItemDisplayRules()
         {
-            return new ItemDisplayRuleDict();
+            ItemBodyModelPrefab = EquipmentModel;
+            ItemBodyModelPrefab.AddComponent<ItemDisplay>();
+            ItemBodyModelPrefab.GetComponent<ItemDisplay>().rendererInfos = ItemHelpers.ItemDisplaySetup(ItemBodyModelPrefab);
+
+            Vector3 generalScale = new Vector3(0.05f, 0.05f, 0.05f);
+            ItemDisplayRuleDict rules = new ItemDisplayRuleDict(new ItemDisplayRule[]
+            {
+                new ItemDisplayRule
+                {
+                    ruleType = ItemDisplayRuleType.ParentedPrefab,
+                    followerPrefab = ItemBodyModelPrefab,
+                    childName = "Head",
+                    localPos = new Vector3(0, 0, 0),
+                    localAngles = new Vector3(0, 0, 0),
+                    localScale = new Vector3(0, 0, 0)
+                }
+            });
+            rules.Add("mdlCommandoDualies", new ItemDisplayRule[]
+            {
+                new ItemDisplayRule
+                {
+                    ruleType = ItemDisplayRuleType.ParentedPrefab,
+                    followerPrefab = ItemBodyModelPrefab,
+                    childName = "Head",
+                    localPos = new Vector3(0.0001F, 0.1785F, 0.2069F),
+                    localAngles = new Vector3(0F, 0F, 0F),
+                    localScale = new Vector3(0.1F, 0.1F, 0.1F)
+                }
+            });
+            rules.Add("mdlHuntress", new ItemDisplayRule[]
+            {
+                new ItemDisplayRule
+                {
+                    ruleType = ItemDisplayRuleType.ParentedPrefab,
+                    followerPrefab = ItemBodyModelPrefab,
+childName = "Head",
+localPos = new Vector3(0.1F, 0.1849F, 0.0903F),
+localAngles = new Vector3(339.1738F, 0F, 90F),
+localScale = new Vector3(0.08F, 0.08F, 0.08F)
+                }
+            });
+            rules.Add("mdlToolbot", new ItemDisplayRule[]
+            {
+                new ItemDisplayRule
+                {
+                    ruleType = ItemDisplayRuleType.ParentedPrefab,
+                    followerPrefab = ItemBodyModelPrefab,
+childName = "Head",
+localPos = new Vector3(0.0642F, 3.5153F, -0.51F),
+localAngles = new Vector3(60F, 0F, 0F),
+localScale = new Vector3(0.3033F, 0.3033F, 0.3033F)
+                }
+            });
+            rules.Add("mdlEngi", new ItemDisplayRule[]
+            {
+                new ItemDisplayRule
+                {
+                    ruleType = ItemDisplayRuleType.ParentedPrefab,
+                    followerPrefab = ItemBodyModelPrefab,
+                    childName = "Chest",
+                    localPos = new Vector3(0f, 0.5f, 0.22f),
+                    localAngles = new Vector3(0f, 1f, -0.06f),
+                    localScale = generalScale
+                }
+            });
+
+            rules.Add("mdlEngiTurret", new ItemDisplayRule[] //NOPE
+            {
+                new ItemDisplayRule
+                {
+                    ruleType = ItemDisplayRuleType.ParentedPrefab,
+                    followerPrefab = ItemBodyModelPrefab,
+                    childName = "Head",
+                    localPos = new Vector3(0f, 0f, 0.25f),
+                    localAngles = new Vector3(0f, 1f, -0.06f),
+                    localScale = generalScale * 5f
+                }
+            });
+            rules.Add("mdlMage", new ItemDisplayRule[]
+            {
+                new ItemDisplayRule
+                {
+                    ruleType = ItemDisplayRuleType.ParentedPrefab,
+                    followerPrefab = ItemBodyModelPrefab,
+                    childName = "Head",
+                    localPos = new Vector3(0f, 0f, 0.13f),
+                    localAngles = new Vector3(0f, 0f, 0f),
+                    localScale = generalScale
+                }
+            });
+            rules.Add("mdlMerc", new ItemDisplayRule[]
+            {
+                new ItemDisplayRule
+                {
+                    ruleType = ItemDisplayRuleType.ParentedPrefab,
+                    followerPrefab = ItemBodyModelPrefab,
+childName = "Head",
+localPos = new Vector3(0F, 0.1F, 0.1705F),
+localAngles = new Vector3(0F, 0F, 0F),
+localScale = new Vector3(0.05F, 0.05F, 0.05F)
+                }
+            });
+            rules.Add("mdlTreebot", new ItemDisplayRule[]
+            {
+                new ItemDisplayRule
+                {
+                    ruleType = ItemDisplayRuleType.ParentedPrefab,
+                    followerPrefab = ItemBodyModelPrefab,
+childName = "HeadBase",
+localPos = new Vector3(0F, 0.628F, -0.392F),
+localAngles = new Vector3(344.4032F, 180F, 180F),
+localScale = new Vector3(0.1341F, 0.1341F, 0.1341F)
+                }
+            });
+            rules.Add("mdlLoader", new ItemDisplayRule[]
+            {
+                new ItemDisplayRule
+                {
+                    ruleType = ItemDisplayRuleType.ParentedPrefab,
+                    followerPrefab = ItemBodyModelPrefab,
+childName = "Head",
+localPos = new Vector3(0F, 0.0501F, 0.1545F),
+localAngles = new Vector3(0F, 0F, 0F),
+localScale = new Vector3(0.05F, 0.05F, 0.05F)
+                }
+            });
+            rules.Add("mdlCroco", new ItemDisplayRule[]
+            {
+                new ItemDisplayRule
+                {
+                    ruleType = ItemDisplayRuleType.ParentedPrefab,
+                    followerPrefab = ItemBodyModelPrefab,
+                    childName = "Head",
+                    localPos = new Vector3(0f, 5.2f, 0.3f),
+                    localAngles = new Vector3(90f, 0f, 0f),
+                    localScale = generalScale * 8
+                }
+            });
+            rules.Add("mdlCaptain", new ItemDisplayRule[]
+            {
+                new ItemDisplayRule
+                {
+                    ruleType = ItemDisplayRuleType.ParentedPrefab,
+                    followerPrefab = ItemBodyModelPrefab,
+childName = "Head",
+localPos = new Vector3(0.0027F, 0.0418F, 0.1528F),
+localAngles = new Vector3(0F, 0F, 0F),
+localScale = new Vector3(0.05F, 0.05F, 0.05F)
+                }
+            });
+            rules.Add("mdlBrother", new ItemDisplayRule[]
+            {
+                new ItemDisplayRule
+                {
+                    ruleType = ItemDisplayRuleType.ParentedPrefab,
+                    followerPrefab = ItemBodyModelPrefab,
+childName = "Head",
+localPos = new Vector3(0F, -0.0616F, 0.18F),
+localAngles = new Vector3(0F, 0F, 0F),
+localScale = new Vector3(0.05F, 0.05F, 0.05F)
+                }
+            });
+            rules.Add("mdlBandit", new ItemDisplayRule[]
+            {
+                new ItemDisplayRule
+                {
+                    ruleType = ItemDisplayRuleType.ParentedPrefab,
+                    followerPrefab = ItemBodyModelPrefab,
+                    childName = "Head",
+                    localPos = new Vector3(0f, 0.15f, 0.12f),
+                    localAngles = new Vector3(-20f, 0f, 0f),
+                    localScale = generalScale
+                }
+            });
+            rules.Add("mdlBandit2", new ItemDisplayRule[]
+            {
+                new ItemDisplayRule
+                {
+                    ruleType = ItemDisplayRuleType.ParentedPrefab,
+                    followerPrefab = ItemBodyModelPrefab,
+                    childName = "Head",
+                    localPos = new Vector3(0F, 0.00001F, 0.12369F),
+                    localAngles = new Vector3(0F, 0F, 0F),
+                    localScale = new Vector3(0.05F, 0.05F, 0.05F)
+                }
+            });
+            rules.Add("mdlClayBruiser", new ItemDisplayRule[]
+            {
+                new ItemDisplayRule
+                {
+                    ruleType = ItemDisplayRuleType.ParentedPrefab,
+                    followerPrefab = ItemBodyModelPrefab,
+                    childName = "Head",
+                    localPos = new Vector3(0f, 0.1f, 0.4f),
+                    localAngles = new Vector3(0f, 0f, 0f),
+                    localScale = generalScale * 2f
+                }
+            });
+            rules.Add("mdlHAND", new ItemDisplayRule[]
+            {
+                new ItemDisplayRule
+                {
+                    ruleType = ItemDisplayRuleType.ParentedPrefab,
+                    followerPrefab = ItemBodyModelPrefab,
+                    childName = "Head",
+                    localPos = new Vector3(0f, 0f, 2.4f),
+                    localAngles = new Vector3(0f, 0f, 0f),
+                    localScale = generalScale * 10f
+                }
+            });
+            rules.Add("mdlScav", new ItemDisplayRule[]
+            {
+                new ItemDisplayRule
+                {
+                    ruleType = ItemDisplayRuleType.ParentedPrefab,
+                    followerPrefab = ItemBodyModelPrefab,
+childName = "Head",
+localPos = new Vector3(0F, 4.8685F, 0.0438F),
+localAngles = new Vector3(288.4044F, 180F, 180F),
+localScale = new Vector3(1F, 1F, 1F)
+                }
+            }); rules.Add("mdlBeetle", new ItemDisplayRule
+            {
+                ruleType = ItemDisplayRuleType.ParentedPrefab,
+                followerPrefab = ItemBodyModelPrefab,
+                childName = "Head",
+                localPos = new Vector3(0.0013F, 0.1559F, -0.2403F),
+                localAngles = new Vector3(0F, 0F, 0F),
+                localScale = new Vector3(0.1F, 0.1F, 0.1F)
+            });
+            rules.Add("mdlLemurian", new ItemDisplayRule
+            {
+                ruleType = ItemDisplayRuleType.ParentedPrefab,
+                followerPrefab = ItemBodyModelPrefab,
+                childName = "Head",
+                localPos = new Vector3(-0.1594F, 3.6456F, 0.0645F),
+                localAngles = new Vector3(279.4401F, 195.4454F, 161.8801F),
+                localScale = new Vector3(0.4099F, 0.4099F, 0.4099F)
+            });
+            rules.Add("mdlLunarGolem", new ItemDisplayRule
+            {
+                ruleType = ItemDisplayRuleType.ParentedPrefab,
+                followerPrefab = ItemBodyModelPrefab,
+                childName = "MuzzleLB",
+                localPos = new Vector3(-1.6752F, -0.2F, -0.468F),
+                localAngles = new Vector3(2.6768F, 179.4175F, 179.4478F),
+                localScale = new Vector3(0.1793F, 0.1793F, 0.1793F)
+            });
+            rules.Add("mdlNullifier", new ItemDisplayRule
+            {
+                ruleType = ItemDisplayRuleType.ParentedPrefab,
+                followerPrefab = ItemBodyModelPrefab,
+                childName = "Muzzle",
+                localPos = new Vector3(0.0002F, -0.189F, 1.9457F),
+                localAngles = new Vector3(24.2706F, 0.0024F, 0.024F),
+                localScale = new Vector3(0.2908F, 0.2908F, 0.2908F)
+            });
+            rules.Add("mdlGravekeeper", new ItemDisplayRule
+            {
+                ruleType = ItemDisplayRuleType.ParentedPrefab,
+                followerPrefab = ItemBodyModelPrefab,
+                childName = "Mask",
+                localPos = new Vector3(0F, 0.0344F, -1.6055F),
+                localAngles = new Vector3(88.6293F, 0F, 0F),
+                localScale = new Vector3(0.425F, 0.425F, 0.425F)
+            });
+            return rules;
         }
 
         protected override bool ActivateEquipment(EquipmentSlot slot)
@@ -42,8 +325,34 @@ namespace RiskOfBulletstormRewrite.Equipment
             var attackerGameObject = slot.characterBody.gameObject;
             foreach (var charMaster in CharacterMaster.readOnlyInstancesList)
             {
-                if (charMaster.playerCharacterMasterController)
+                //todo:
+                // Should something happen to a player. obv not TrueKill, but...?
+                if (charMaster.playerCharacterMasterController && charMaster.teamIndex != slot.teamComponent.teamIndex && charMaster.GetBody())
+                {
+                    var charBody = charMaster.GetBody();
+                    switch (cfgEnemyPlayerDamageType.Value)
+                    {
+                        case IronCoinEnum.Nonfatal:
+                        case IronCoinEnum.Fatal:
+                            DamageInfo ironCoinDamage = new DamageInfo()
+                            {
+                                attacker = slot.gameObject,
+                                crit = false,
+                                damage = charBody.healthComponent.fullCombinedHealth * cfgDamageToPlayers.Value,
+                                damageType = cfgEnemyPlayerDamageType.Value == IronCoinEnum.Nonfatal ? DamageType.NonLethal : DamageType.Generic,
+                                position = charBody.corePosition,
+                                procCoefficient = 0,
+                            };
+                            charBody.healthComponent.TakeDamage(ironCoinDamage);
+                            enemiesKilled++;
+                            break;
+                        default:
+                            charBody.healthComponent.Suicide(slot.gameObject);
+                            enemiesKilled++;
+                            break;
+                    }
                     continue;
+                }
 
                 var body = charMaster.GetBody();
                 if (charMaster.inventory && body && body.healthComponent && !body.isBoss && body.healthComponent.alive && charMaster.teamIndex != slot.teamComponent.teamIndex)
@@ -75,8 +384,8 @@ namespace RiskOfBulletstormRewrite.Equipment
             }
             if (enemiesKilled > 0)
             {
-                CharacterMasterNotificationQueue.PushEquipmentTransformNotification(slot.characterBody.master, slot.characterBody.inventory.currentEquipmentIndex, IronCoinConsumed.instance.EquipmentDef.equipmentIndex, CharacterMasterNotificationQueue.TransformationType.Default);
-                slot.inventory.SetEquipmentIndex(IronCoinConsumed.instance.EquipmentDef.equipmentIndex);
+                CharacterMasterNotificationQueue.PushEquipmentTransformNotification(slot.characterBody.master, slot.characterBody.inventory.currentEquipmentIndex, IronCoinConsumed.Instance.EquipmentDef.equipmentIndex, CharacterMasterNotificationQueue.TransformationType.Default);
+                slot.inventory.SetEquipmentIndex(IronCoinConsumed.Instance.EquipmentDef.equipmentIndex);
                 return true;
             }
             return false;
--- RiskOfBulletstormRewrite/Equipment/Lunar/IronCoinConsumed.cs
+++ RiskOfBulletstormRewrite/Equipment/Lunar/IronCoinConsumed.cs
@@ -1,5 +1,6 @@
 ﻿using BepInEx.Configuration;
 using R2API;
+using RiskOfBulletstormRewrite.Utils;
 using RoR2;
 using UnityEngine;
 
@@ -7,7 +8,7 @@ namespace RiskOfBulletstormRewrite.Equipment
 {
     public class IronCoinConsumed : EquipmentBase<IronCoinConsumed>
     {
-        //public static ConfigEntry<float> cfg;
+        public static ConfigEntry<bool> cfgResultPrintByPlayersOnly;
 
         public override string EquipmentName => "Iron Coin (Spent)";
 
@@ -41,25 +42,51 @@ namespace RiskOfBulletstormRewrite.Equipment
 
         protected override void CreateConfig(ConfigFile config)
         {
+            cfgResultPrintByPlayersOnly = config.Bind(ConfigCategory, "Only Players Can Print Result", false, "If true, then only players can print the result of the flip.");
         }
 
         public override ItemDisplayRuleDict CreateItemDisplayRules()
         {
-            return new ItemDisplayRuleDict();
+            ItemBodyModelPrefab = EquipmentModel;
+            ItemBodyModelPrefab.AddComponent<ItemDisplay>();
+            ItemBodyModelPrefab.GetComponent<ItemDisplay>().rendererInfos = ItemHelpers.ItemDisplaySetup(ItemBodyModelPrefab);
+
+            Vector3 generalScale = new Vector3(0.05f, 0.05f, 0.05f);
+            ItemDisplayRuleDict rules = new ItemDisplayRuleDict(new ItemDisplayRule[]
+            {
+                new ItemDisplayRule
+                {
+                    ruleType = ItemDisplayRuleType.ParentedPrefab,
+                    followerPrefab = ItemBodyModelPrefab,
+                    childName = "Head",
+                    localPos = new Vector3(0, 0, 0),
+                    localAngles = new Vector3(0, 0, 0),
+                    localScale = new Vector3(0, 0, 0)
+                }
+            });
+            return rules;
         }
 
         protected override bool ActivateEquipment(EquipmentSlot slot)
         {
             if (slot.characterBody)
             {
-                //RISKOFBULLETSTORM_EQUIPMENT_IRONCOINCONSUMED_FLAVORSIDE maybe
-                var result = Util.CheckRoll(50f) ? "RISKOFBULLETSTORM_EQUIPMENT_IRONCOINCONSUMED_FLAVORHEADS" : "RISKOFBULLETSTORM_EQUIPMENT_IRONCOINCONSUMED_FLAVORTAILS";
-                Chat.SendBroadcastChat(new Chat.SubjectFormatChatMessage
+                var result = Util.CheckRoll(1f) 
+                    ? "RISKOFBULLETSTORM_EQUIPMENT_IRONCOINCONSUMED_FLAVORSIDE"
+                    : Util.CheckRoll(50f)
+                    ? "RISKOFBULLETSTORM_EQUIPMENT_IRONCOINCONSUMED_FLAVORHEADS" 
+                    : "RISKOFBULLETSTORM_EQUIPMENT_IRONCOINCONSUMED_FLAVORTAILS";
+                
+                if ((cfgResultPrintByPlayersOnly.Value && slot.characterBody.isPlayerControlled)
+                    || !cfgResultPrintByPlayersOnly.Value)
                 {
-                    baseToken = "RISKOFBULLETSTORM_EQUIPMENT_IRONCOINCONSUMED_FLAVOR",
-                    paramTokens = new string[] { result },
-                    subjectAsCharacterBody = slot.characterBody
-                });
+                    Chat.SendBroadcastChat(new Chat.SubjectFormatChatMessage
+                    {
+                        baseToken = "RISKOFBULLETSTORM_EQUIPMENT_IRONCOINCONSUMED_FLAVOR",
+                        paramTokens = new string[] { result },
+                        subjectAsCharacterBody = slot.characterBody
+                    });
+                }
                 slot.subcooldownTimer = 1f;
             }
             return true;
