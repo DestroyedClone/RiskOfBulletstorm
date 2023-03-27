2464fb374f4437464658598e0742b7b73aded73a
DestroyedClone <destroyedclone@gmail.com>
2023-03-26 22:30:14 -0700

    Created tally item for WeirdEgg

---
+++ RiskOfBulletstormRewrite/Items/NoTier/WeirdEggStageClearTally.cs
@@ -0,0 +1,44 @@
+﻿using BepInEx.Configuration;
+using R2API;
+using RoR2;
+using UnityEngine;
+
+namespace RiskOfBulletstormRewrite.Items
+{
+    public class WeirdEggStageClearTally : ItemBase<WeirdEggStageClearTally>
+    {
+        public override string ItemName => "WeirdEggStageClearTally";
+
+        public override string ItemLangTokenName => "WeirdEggStageClearTally";
+
+        public override ItemTier Tier => ItemTier.NoTier;
+
+        public override GameObject ItemModel => Assets.NullModel;
+
+        public override Sprite ItemIcon => LoadSprite();
+
+        public override string ParentEquipmentName => "WeirdEgg";
+
+        public override bool Hidden => true;
+
+        public override ItemTag[] ItemTags => new ItemTag[]
+        {
+        };
+
+        public override void Init(ConfigFile config)
+        {
+            CreateLang();
+            CreateItem();
+            Hooks();
+        }
+
+        public override ItemDisplayRuleDict CreateItemDisplayRules()
+        {
+            return new ItemDisplayRuleDict();
+        }
+
+        public override void Hooks()
+        {
+        }
+    }
+}
