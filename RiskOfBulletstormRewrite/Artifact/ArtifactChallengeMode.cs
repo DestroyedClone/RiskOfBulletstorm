5b1699b9d6fb39098b4b8a993b5cd35951d9ccae
DestroyedClone <destroyedclone@gmail.com>
2023-04-24 10:11:54 -0700

    pre-stuff

---
+++ RiskOfBulletstormRewrite/Artifact/ArtifactChallengeMode.cs
@@ -0,0 +1,48 @@
+﻿using BepInEx.Configuration;
+using RoR2;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace RiskOfBulletstormRewrite.Artifact
+{
+    internal class ArtifactChallengeMode : ArtifactBase<ArtifactChallengeMode>
+    {
+        public override string ArtifactName => "Artifact of Daisuke";
+
+        public override string ArtifactLangTokenName => "CHALLENGEMODE";
+
+        public override Sprite ArtifactEnabledIcon => LoadSprite(true);
+
+        public override Sprite ArtifactDisabledIcon => LoadSprite(false);
+
+        public override void Init(ConfigFile config)
+        {
+            CreateLang();
+            CreateArtifact();
+            Hooks();
+        }
+
+        public override void Hooks()
+        {
+            RunArtifactManager.onArtifactEnabledGlobal += RunArtifactManager_onArtifactEnabledGlobal;
+            RunArtifactManager.onArtifactDisabledGlobal += RunArtifactManager_onArtifactDisabledGlobal;
+        }
+
+        private void RunArtifactManager_onArtifactDisabledGlobal([JetBrains.Annotations.NotNull] RunArtifactManager runArtifactManager, [JetBrains.Annotations.NotNull] ArtifactDef artifactDef)
+        {
+            if (artifactDef != ArtifactDef)
+            {
+                return;
+            }
+        }
+
+        private void RunArtifactManager_onArtifactEnabledGlobal([JetBrains.Annotations.NotNull] RunArtifactManager runArtifactManager, [JetBrains.Annotations.NotNull] ArtifactDef artifactDef)
+        {
+            if (artifactDef != ArtifactDef)
+            {
+                return;
+            }
+        }
+
+    }
+}
--- RiskOfBulletstormRewrite/Items/Uncommon/AmmoBelt.cs
+++ RiskOfBulletstormRewrite/Items/Uncommon/AmmoBelt.cs
@@ -1,5 +1,6 @@
 ﻿using BepInEx.Configuration;
 using R2API;
+using Rewired.Utils;
 using RiskOfBulletstormRewrite.Modules;
 using RoR2;
 using UnityEngine;
@@ -9,6 +10,8 @@ namespace RiskOfBulletstormRewrite.Items
     public class AmmoBelt : ItemBase<AmmoBelt>
     {
         public static ConfigEntry<float> cfgPercentageStockAdditive;
+        public static ConfigEntry<int> cfgAmmoPackAdditionalRecharge;
+        public static ConfigEntry<int> cfgAmmoPackAdditionalRechargeStack;
 
         public override string ItemName => "Ammo Belt";
 
@@ -17,6 +20,8 @@ namespace RiskOfBulletstormRewrite.Items
         public override string[] ItemFullDescriptionParams => new string[]
         {
             GetChance(cfgPercentageStockAdditive),
+            cfgAmmoPackAdditionalRecharge.Value.ToString(),
+            cfgAmmoPackAdditionalRechargeStack.Value.ToString()
         };
 
         public override ItemTier Tier => ItemTier.Tier2;
@@ -40,7 +45,9 @@ namespace RiskOfBulletstormRewrite.Items
 
         public override void CreateConfig(ConfigFile config)
         {
-            cfgPercentageStockAdditive = config.Bind(ConfigCategory, "Percentage", 0.09f, "This value is rounded when giving stocks.");
+            cfgPercentageStockAdditive = config.Bind(ConfigCategory, "Percentage", 0.15f, "This value is rounded when giving stocks.");
+            cfgAmmoPackAdditionalRecharge = config.Bind(ConfigCategory, "Ammo Pack Additional Stock", 1, "This value is rounded when giving stocks.");
+            cfgAmmoPackAdditionalRechargeStack = config.Bind(ConfigCategory, "Ammo Pack Additional Stock", 1, "This value is rounded when giving stocks.");
         }
 
         public override ItemDisplayRuleDict CreateItemDisplayRules()
@@ -51,11 +58,17 @@ namespace RiskOfBulletstormRewrite.Items
         public override void Hooks()
         {
             On.RoR2.GenericSkill.SetBonusStockFromBody += AddAmmoBeltStocks;
+            On.RoR2.SkillLocator.ApplyAmmoPack += SkillLocator_ApplyAmmoPack;
+        }
+
+        private void SkillLocator_ApplyAmmoPack(On.RoR2.SkillLocator.orig_ApplyAmmoPack orig, SkillLocator self)
+        {
+            throw new System.NotImplementedException();
         }
 
         public void AddAmmoBeltStocks(On.RoR2.GenericSkill.orig_SetBonusStockFromBody orig, GenericSkill self, int newBonusStockFromBody)
         {
-            if (self.baseStock > 0)
+            if (self.baseStock > 1)
             {
                 var characterBody = self.characterBody;
                 if (characterBody)
@@ -63,14 +76,15 @@ namespace RiskOfBulletstormRewrite.Items
                     var itemCount = GetCount(characterBody);
                     if (itemCount > 0)
                     {
-                        int stocksToGive = 0;
+                        int stocksToGive;
                         if (itemCount == 1)
                         {
                             stocksToGive = 1;
                         } else
                         {
                             var multipliedValue = itemCount * cfgPercentageStockAdditive.Value;
-                            newBonusStockFromBody = Mathf.RoundToInt(multipliedValue);
+                            stocksToGive = Mathf.RoundToInt(multipliedValue);
+                            stocksToGive = Mathf.Max(1, stocksToGive);
                         }
                         newBonusStockFromBody += stocksToGive;
                     }
