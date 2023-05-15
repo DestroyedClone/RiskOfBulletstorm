1970ab463722c3c886745363663e14063365ea2a
DestroyedClone <destroyedclone@gmail.com>
2023-05-15 01:51:34 -0700

    Alphabullets: Changed comp to be added to the CharacterMaster rather than the CharacterBody to improve syncing

--- RiskOfBulletstormRewrite/Items/Uncommon/AlphaBullets.cs
+++ RiskOfBulletstormRewrite/Items/Uncommon/AlphaBullets.cs
@@ -54,8 +54,16 @@ namespace RiskOfBulletstormRewrite.Items
 
         public override void Hooks()
         {
-            CharacterBody.onBodyStartGlobal += AddComp;
             RecalculateStatsAPI.GetStatCoefficients += StatCoefficients;
+            CharacterMaster.onStartGlobal += CharacterMaster_onStartGlobal;
+        }
+
+        private void CharacterMaster_onStartGlobal(CharacterMaster obj)
+        {
+            if (NetworkServer.active && obj.inventory)
+            {
+                obj.gameObject.AddComponent<RBSAlphaBulletController>().characterMaster = obj;
+            }
         }
 
         private void StatCoefficients(CharacterBody sender, RecalculateStatsAPI.StatHookEventArgs args)
@@ -73,37 +81,47 @@ namespace RiskOfBulletstormRewrite.Items
             }
         }
 
-        public void AddComp(CharacterBody body)
-        {
-            if (NetworkServer.active && body.inventory)
-            {
-                body.gameObject.AddComponent<RBS_AlphaBullet>();
-            }
-        }
-
-        public class RBS_AlphaBullet : MonoBehaviour
+        public class RBSAlphaBulletController : MonoBehaviour
         {
             public CharacterBody body;
+            public CharacterMaster characterMaster;
 
             public GenericSkill primary = null;
             public GenericSkill secondary = null;
             public GenericSkill utility = null;
             public GenericSkill special = null;
 
+            public BuffIndex AlphaBulletBuffIndex => Utils.Buffs.AlphaBulletBuff.buffIndex;
+
             public bool hasItem = false;
+
 
             public void Start()
             {
-                if (!body)
+                body = characterMaster.GetBody();
+                if (body)
                 {
-                    body = gameObject.GetComponent<CharacterBody>();
-                }
-                if (!body.skillLocator)
-                {
-                    enabled = false;
                     return;
                 }
-                else
+                characterMaster.onBodyStart += CharacterMaster_onBodyStart;
+                characterMaster.onBodyDestroyed += CharacterMaster_onBodyDestroyed;
+                characterMaster.inventory.onInventoryChanged += Inventory_onInventoryChanged;
+            }
+
+            private void Inventory_onInventoryChanged()
+            {
+                hasItem = instance.GetCount(characterMaster) > 0;
+            }
+
+            private void CharacterMaster_onBodyDestroyed(CharacterBody obj)
+            {
+                body = null;
+            }
+
+            private void CharacterMaster_onBodyStart(CharacterBody obj)
+            {
+                body = obj;
+                if (body.skillLocator)
                 {
                     var sk = body.skillLocator;
                     primary = sk.primary;
@@ -111,39 +129,59 @@ namespace RiskOfBulletstormRewrite.Items
                     utility = sk.utility;
                     special = sk.special;
                 }
-                body.onInventoryChanged += OnInventoryChanged;
-                OnInventoryChanged();
             }
 
             public void OnDestroy()
             {
-                body.onInventoryChanged -= OnInventoryChanged;
-            }
-
-            public void OnInventoryChanged()
-            {
-                hasItem = body.inventory.GetItemCount(instance.ItemDef) > 0;
-                if (hasItem)
-                {
-                    enabled = true;
-                }
-                else
-                {
-                    enabled = false;
-                }
+                characterMaster.onBodyStart -= CharacterMaster_onBodyStart;
+                characterMaster.onBodyDestroyed -= CharacterMaster_onBodyDestroyed;
+                characterMaster.inventory.onInventoryChanged -= Inventory_onInventoryChanged;
             }
 
             public void OnDisable()
             {
-                body.SetBuffCount(Utils.Buffs.AlphaBulletBuff.buffIndex, 0);
+                SetBuffCount(0);
             }
+
+            public void SetBuffCount(int count)
+            {
+                if (body == null)
+                {
+                    return;
+                }
+
+                var currentBuffCount = body.GetBuffCount(AlphaBulletBuffIndex);
+                if (currentBuffCount == count)
+                {
+                    return;
+                }
+
+                if (currentBuffCount > count)
+                {
+                    for (int i = 0; i < currentBuffCount - count; i++)
+                    {
+                        body.RemoveBuff(AlphaBulletBuffIndex);
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < count - currentBuffCount; i++)
+                    {
+                        body.AddBuff(AlphaBulletBuffIndex);
+                    }
+                }
+            }
+
 
             public void FixedUpdate()
             {
                 if (!hasItem)
+                {
+                    SetBuffCount(0);
                     return;
-                var validSkills = Inc(primary) + Inc(secondary) + Inc(utility) + Inc(special);
-                body.SetBuffCount(Utils.Buffs.AlphaBulletBuff.buffIndex, validSkills);
+                }
+                var validSkillCount = Inc(primary) + Inc(secondary) + Inc(utility) + Inc(special);
+                SetBuffCount(validSkillCount);
             }
 
             public int Inc(GenericSkill gs)
